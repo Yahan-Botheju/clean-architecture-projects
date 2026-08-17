474877091d959246274a51773c457a8994fa170a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 21:02:25 +0530

    feat: initiate order persistence mapper

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/persistenceMapper/OrderPersistenceMapper.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/persistenceMapper/OrderPersistenceMapper.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.persistenceMapper;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.entities.OrderEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface OrderPersistenceMapper {
+    //domain model to entity
+    OrderEntity toEntity(Order order);
+
+    //entity to domain model
+    Order toDomainModel(OrderEntity orderEntity);
 }
