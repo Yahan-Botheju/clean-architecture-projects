0287ee761764b3cd725ccca8bbc57e7c34c1d329
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 20:48:29 +0530

    feat: initiate order domain model

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/domain/modules/Order.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/domain/modules/Order.java
@@ -1,4 +1,31 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.enums.OrderStatus;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
 public class Order {
+    private final UUID orderId;
+    private final UUID customerId;
+    private final UUID productId;
+    private int orderQuantity;
+    private BigDecimal totalPrice;
+    private OrderStatus orderStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Order(UUID orderId, UUID customerId, UUID productId, int orderQuantity, BigDecimal totalPrice, OrderStatus orderStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.orderQuantity = orderQuantity;
+        this.totalPrice = totalPrice;
+        this.orderStatus = orderStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
