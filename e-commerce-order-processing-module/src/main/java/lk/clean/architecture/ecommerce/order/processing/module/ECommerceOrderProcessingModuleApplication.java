c38fe28a054b6507b013c99ebcb69625ae5a238a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 19:39:42 +0530

    add all

---
+++ e-commerce-order-processing-module/src/main/java/lk/clean/architecture/ecommerce/order/processing/module/ECommerceOrderProcessingModuleApplication.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.ecommerce.order.processing.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderProcessingModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceOrderProcessingModuleApplication.class, args);
+    }
+
+}
---
+++ e-commerce-order-processing-module/src/test/java/lk/clean/architecture/ecommerce/order/processing/module/ECommerceOrderProcessingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.ecommerce.order.processing.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOrderProcessingModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
