08d6fd7c54822c6c171d8a713148e532cfdd4324
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 21:09:04 +0530

    feat: initiate order entity

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/entities/OrderEntity.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/entities/OrderEntity.java
@@ -1,4 +1,41 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.entities;
 
+import jakarta.persistence.*;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderId;
+
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @Column(name = "product_id", nullable = false)
+    private UUID productId;
+
+    @Column(name = "order_quantity", nullable = false)
+    private int orderQuantity;
+
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status", nullable = false)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
