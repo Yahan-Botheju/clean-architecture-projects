1f89e9f8d38d4510d56e57119380e6ec8e455e09
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 20:10:05 +0530

    project 2 initial commit

---
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/ECommerceCommerceInventoryAndOrderFulfillmentModuleApplication.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCommerceInventoryAndOrderFulfillmentModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceCommerceInventoryAndOrderFulfillmentModuleApplication.class, args);
+    }
+
+}
---
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/test/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/ECommerceCommerceInventoryAndOrderFulfillmentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCommerceInventoryAndOrderFulfillmentModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
