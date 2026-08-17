2a4bb9bcf347ca43dfeeee31ad96f12ec07c5fb2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 20:45:31 +0530

    feat: initiate order module enum

---
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/domain/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    CANCELED
+}
