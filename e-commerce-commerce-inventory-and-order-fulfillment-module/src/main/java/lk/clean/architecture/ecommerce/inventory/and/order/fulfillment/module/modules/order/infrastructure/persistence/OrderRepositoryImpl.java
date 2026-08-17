7b7e567a9d1b2b12d344cd5722a48b460e9bb2dd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-17 21:06:20 +0530

    feat: inject required dependencies

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/OrderRepositoryImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/OrderRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence;
 
-public class OrderRepositoryImpl {
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.repositories.OrderRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.jpa.JpaOrderRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.persistenceMapper.OrderPersistenceMapper;
+
+public class OrderRepositoryImpl implements OrderRepository {
+
+    //inject required dependencies
+    private final JpaOrderRepository jpaOrderRepository;
+    private final OrderPersistenceMapper orderPersistenceMapper;
+
+    public OrderRepositoryImpl(
+            JpaOrderRepository jpaOrderRepository,
+            OrderPersistenceMapper orderPersistenceMapper
+    ) {
+        this.jpaOrderRepository = jpaOrderRepository;
+        this.orderPersistenceMapper = orderPersistenceMapper;
+    }
 }
