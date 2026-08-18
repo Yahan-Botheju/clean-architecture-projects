6da2a52d8dc236b150fec9debc8394f09621553b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-18 21:48:24 +0530

    feat: inject required dependencies

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/persistence/InventoryItemRepositoryImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/persistence/InventoryItemRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence;
 
-public class InventoryItemRepositoryImpl {
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence.jpa.JpaInventoryItemRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence.persistenceMapper.PersistenceMapper;
+
+public class InventoryItemRepositoryImpl implements InventoryItemRepository {
+
+    //inject required dependencies
+    private final JpaInventoryItemRepository jpaInventoryItemRepository;
+    private final PersistenceMapper persistenceMapper;
+
+    public InventoryItemRepositoryImpl(
+            JpaInventoryItemRepository jpaInventoryItemRepository,
+            PersistenceMapper persistenceMapper
+    ) {
+        this.jpaInventoryItemRepository = jpaInventoryItemRepository;
+        this.persistenceMapper = persistenceMapper;
+    }
 }
