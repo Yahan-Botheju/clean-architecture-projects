59b75bb7ee480eb7c0e8df24cfd832f600eb3ab8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 19:14:40 +0530

    feat: create method functionality

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/usecase/InventoryModuleApiImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/usecase/InventoryModuleApiImpl.java
@@ -1,7 +1,12 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase;
 
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.api.InventoryModuleApi;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class InventoryModuleApiImpl implements InventoryModuleApi {
 
@@ -10,5 +15,21 @@ public class InventoryModuleApiImpl implements InventoryModuleApi {
 
     public InventoryModuleApiImpl(InventoryItemRepository inventoryItemRepository) {
         this.inventoryItemRepository = inventoryItemRepository;
+    }
+
+    //stock reservation
+    @Override
+    public void stockReservation(
+            UUID productId,
+            int requestedQuantity,
+            LocalDateTime currentTime
+    ){
+        InventoryItem checkProduct = inventoryItemRepository.findByProductId(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+        //call domain logic
+        checkProduct.stockReservation(requestedQuantity, currentTime);
+
+        inventoryItemRepository.createNewItem(checkProduct);
     }
 }
