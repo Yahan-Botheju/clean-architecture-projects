6df9152c33b2ee7bae3e34fd626be62fe26ae48e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 22:36:55 +0530

    feat: initiate method for get unit price

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/api/InventoryModuleApi.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/api/InventoryModuleApi.java
@@ -1,5 +1,6 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.api;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -7,4 +8,7 @@ public interface InventoryModuleApi {
 
     //RESERVATION METHOD FOR EXTERNAL METHOD USAGE
     public void stockReservation(UUID productId, int requestedQuantity,  LocalDateTime currentTime);
+
+    //unite price
+    BigDecimal calculateTotalPrice(UUID productId);
 }
