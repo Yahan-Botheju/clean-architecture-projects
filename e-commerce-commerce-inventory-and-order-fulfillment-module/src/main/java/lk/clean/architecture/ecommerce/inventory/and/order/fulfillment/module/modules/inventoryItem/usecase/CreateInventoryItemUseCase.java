efcd81bd18e0065d7ae3c09d2ef97a87fcc40e87
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 12:33:53 +0530

    feat: initiate create new item method

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/usecase/CreateInventoryItemUseCase.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/usecase/CreateInventoryItemUseCase.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
+
+import java.math.BigDecimal;
+
 public interface CreateInventoryItemUseCase {
+
+    //create new item
+    InventoryItem createInventoryItem(String productName, int availableStock, BigDecimal unitPrice);
 }
