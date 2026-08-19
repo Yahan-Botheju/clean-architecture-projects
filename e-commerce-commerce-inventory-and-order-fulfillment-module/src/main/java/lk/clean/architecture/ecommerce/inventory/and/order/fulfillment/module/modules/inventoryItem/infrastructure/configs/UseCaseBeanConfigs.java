e221b1f4db51fc0a5b5f46850b96e1eb7ffbe684
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 12:12:29 +0530

    feat: intiate create inventory item usecase impl as service class through config class

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/configs/UseCaseBeanConfigs.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,19 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.configs;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.CreateInventoryItemUseCase;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.CreateInventoryItemUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    //create inventory item impl
+    @Bean
+    public CreateInventoryItemUseCase createInventoryItemUseCase(
+            InventoryItemRepository inventoryItemRepository
+    ) {
+        return new CreateInventoryItemUseCaseImpl(inventoryItemRepository);
+    }
 }
