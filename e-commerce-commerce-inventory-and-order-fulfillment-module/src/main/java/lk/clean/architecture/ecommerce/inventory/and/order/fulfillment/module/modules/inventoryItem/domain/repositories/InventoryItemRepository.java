277df751022d02a82c4787b7c7dbed5be3e8bc64
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 19:07:08 +0530

    feat: initiate method

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/repositories/InventoryItemRepository.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/repositories/InventoryItemRepository.java
@@ -2,7 +2,13 @@ package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.m
 
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface InventoryItemRepository {
+
+    //check product by id
+    Optional<InventoryItem> findByProductId(UUID productId);
 
     //create new item
     InventoryItem createNewItem(InventoryItem inventoryItem);
