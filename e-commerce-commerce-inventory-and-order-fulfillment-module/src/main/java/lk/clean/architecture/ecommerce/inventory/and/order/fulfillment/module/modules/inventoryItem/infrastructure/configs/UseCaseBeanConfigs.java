695f40bfa8b542ee743ecf7727d9b96aeb023b4f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 19:13:29 +0530

    feat: initiate inventory module api impl as service class through config class

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/configs/UseCaseBeanConfigs.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,8 +1,10 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.configs;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.api.InventoryModuleApi;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.CreateInventoryItemUseCase;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.CreateInventoryItemUseCaseImpl;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.InventoryModuleApiImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +17,13 @@ public class UseCaseBeanConfigs {
             InventoryItemRepository inventoryItemRepository
     ) {
         return new CreateInventoryItemUseCaseImpl(inventoryItemRepository);
+    }
+
+    //inventory module api
+    @Bean
+    public InventoryModuleApi inventoryModuleApi(
+            InventoryItemRepository inventoryItemRepository
+    ) {
+        return new InventoryModuleApiImpl(inventoryItemRepository);
     }
 }
