f607413eae6b67e0f0f64472a2edaf2c16980fed
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 11:50:20 +0530

    feat: save item in db

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/persistence/InventoryItemRepositoryImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/infrastructure/persistence/InventoryItemRepositoryImpl.java
@@ -1,6 +1,8 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence.entities.InventoryItemEntity;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence.jpa.JpaInventoryItemRepository;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.infrastructure.persistence.persistenceMapper.PersistenceMapper;
 
@@ -16,5 +18,15 @@ public class InventoryItemRepositoryImpl implements InventoryItemRepository {
     ) {
         this.jpaInventoryItemRepository = jpaInventoryItemRepository;
         this.persistenceMapper = persistenceMapper;
+    }
+
+    //save item
+    @Override
+    public InventoryItem createNewItem(InventoryItem inventoryItem) {
+
+        InventoryItemEntity toEntity = persistenceMapper.toEntity(inventoryItem);
+        InventoryItemEntity savedItem = jpaInventoryItemRepository.save(toEntity);
+
+        return persistenceMapper.toDomainModel(savedItem);
     }
 }
