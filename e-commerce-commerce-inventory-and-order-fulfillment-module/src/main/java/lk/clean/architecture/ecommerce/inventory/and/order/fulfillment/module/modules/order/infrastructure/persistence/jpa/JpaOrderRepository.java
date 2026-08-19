af60327cfed04245f6f4532dae112c4bb9bd2307
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 13:00:30 +0530

    feat: create custom method to get product by product di

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/jpa/JpaOrderRepository.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/infrastructure/persistence/jpa/JpaOrderRepository.java
@@ -3,7 +3,11 @@ package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.m
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.infrastructure.persistence.entities.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaOrderRepository extends JpaRepository<OrderEntity, UUID> {
+
+    //find product by product id
+    Optional<OrderEntity> findByProductId(UUID productId);
 }
