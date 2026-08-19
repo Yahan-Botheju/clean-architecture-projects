846dd70d3b5c824a81c7de2d0be902666df97833
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 11:50:03 +0530

    feat: initiate create new item method

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/repositories/InventoryItemRepository.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/repositories/InventoryItemRepository.java
@@ -1,4 +1,9 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
+
 public interface InventoryItemRepository {
+
+    //create new item
+    InventoryItem createNewItem(InventoryItem inventoryItem);
 }
