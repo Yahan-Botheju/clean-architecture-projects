84fc32ba2c24eccdbe53ebe99e3cad5510a82f9d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-19 11:26:20 +0530

    feat: create stock reservation logic

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/modules/InventoryItem.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/domain/modules/InventoryItem.java
@@ -1,5 +1,6 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.enums.OrderStatus;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -22,5 +23,16 @@ public class InventoryItem {
         this.unitPrice = unitPrice;
         this.reservedStock  = reservedStock;
         this.updatedAt = updatedAt;
+    }
+
+     /* __DOMAIN_BUSINESS_LOGICS__ */
+    //stock reservation
+    public void stockReservation(int requestedQuantity, LocalDateTime currentTime) {
+        if(this.availableStock < requestedQuantity) {
+            throw new IllegalStateException("Available stock is too low" + requestedQuantity);
+        }
+        this.availableStock -= requestedQuantity;
+        this.reservedStock += requestedQuantity;
+        this.updatedAt = currentTime;
     }
 }
