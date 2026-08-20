46d9222070e311f64f50cdc748e9489ee86755c0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-20 11:54:10 +0530

    feat: initiate create new order method

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/PlaceOrderUseCase.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/PlaceOrderUseCase.java
@@ -1,4 +1,11 @@
-package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase;
+package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase;
+
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
+
+import java.util.UUID;
 
 public interface PlaceOrderUseCase {
+
+    //place new order
+    Order placeOrder( UUID customerId, UUID productId, int requestQuantity);
 }
