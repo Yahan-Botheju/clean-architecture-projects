874dec1b9e0b1e856c4d246e9de5a29bf8825347
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-20 12:21:58 +0530

    feat: initiate confirm order method

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ConfirmOrderUseCase.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ConfirmOrderUseCase.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
+
+import java.util.UUID;
+
 public interface ConfirmOrderUseCase {
+
+    //confirm order
+    Order confirmOrder(UUID orderId);
 }
