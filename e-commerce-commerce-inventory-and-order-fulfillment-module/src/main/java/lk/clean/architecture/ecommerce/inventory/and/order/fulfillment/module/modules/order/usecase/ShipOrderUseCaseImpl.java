b12b848b299ef9cc6e86254eef755185bffe048c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-22 20:45:05 +0530

    feat: injecr required dependencies

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ShipOrderUseCaseImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ShipOrderUseCaseImpl.java
@@ -1,4 +1,32 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase;
 
-public class ShipOrderUseCaseImpl {
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.api.InventoryModuleApi;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.repositories.OrderRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
+
+public class ShipOrderUseCaseImpl implements ShipOrderUseCase {
+
+    //inject required dependencies
+    private final OrderRepository orderRepository;
+    private final InventoryModuleApi inventoryModuleApi;
+
+    public ShipOrderUseCaseImpl(
+            OrderRepository orderRepository,
+            InventoryModuleApi inventoryModuleApi
+    ) {
+        this.orderRepository = orderRepository;
+        this.inventoryModuleApi = inventoryModuleApi;
+    }
+
+    public Order shipOrder(UUID orderId) {
+
+        Order chcekOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order Not Found"));
+
+
+    }
 }
