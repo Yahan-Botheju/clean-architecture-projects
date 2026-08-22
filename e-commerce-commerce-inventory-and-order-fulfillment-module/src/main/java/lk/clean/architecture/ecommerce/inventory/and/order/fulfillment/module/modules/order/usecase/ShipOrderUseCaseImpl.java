38570c92b796b63bdcc2a5621ff0310c7900edba
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-22 21:22:59 +0530

    feat: initiate ship order usecase impl

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ShipOrderUseCaseImpl.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/usecase/ShipOrderUseCaseImpl.java
@@ -1,11 +1,11 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase;
 
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.api.InventoryModuleApi;
-import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.repositories.InventoryItemRepository;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.repositories.OrderRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class ShipOrderUseCaseImpl implements ShipOrderUseCase {
@@ -22,11 +22,25 @@ public class ShipOrderUseCaseImpl implements ShipOrderUseCase {
         this.inventoryModuleApi = inventoryModuleApi;
     }
 
-    public Order shipOrder(UUID orderId) {
-
+    //ship order
+    @Override
+    public Order shipOrder(UUID orderId){
+        //check order
         Order chcekOrder = orderRepository.findById(orderId)
                 .orElseThrow(() -> new ResourceNotFoundException("Order Not Found"));
 
+        //use domain logic
+        LocalDateTime currentTime = LocalDateTime.now();
+        chcekOrder.shipCheck(currentTime);
 
+        //use inventory domain for product existence
+        inventoryModuleApi.deductStock(
+                chcekOrder.getProductId(),
+                chcekOrder.getOrderQuantity(),
+                currentTime
+        );
+
+        //save order
+        return orderRepository.save(chcekOrder);
     }
 }
