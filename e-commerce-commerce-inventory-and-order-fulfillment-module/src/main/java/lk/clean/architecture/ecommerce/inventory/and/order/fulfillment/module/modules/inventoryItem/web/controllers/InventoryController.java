cd1cbeb839bce0fd1b87c0971f0df9a449a8a46a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-23 20:20:54 +0530

    feat: setup create inventory item endpoint

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/web/controllers/InventoryController.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/web/controllers/InventoryController.java
@@ -1,8 +1,15 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.controllers;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.CreateInventoryItemUseCase;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.usecase.RestockInventoryUseCase;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.DTOs.CreateInventoryRequestDTO;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.DTOs.InventoryResponseDTO;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.webMappers.InventoryWebMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +30,21 @@ public class InventoryController {
         this.createInventoryItemUseCase = createInventoryItemUseCase;
         this.restockInventoryUseCase = restockInventoryUseCase;
         this.inventoryWebMapper = inventoryWebMapper;
+    }
+
+    //create inventory
+    @PostMapping
+    public ResponseEntity<InventoryResponseDTO> createInventoryItem(
+            @RequestBody CreateInventoryRequestDTO createInventoryRequestDTO
+            ){
+        //set values to usecase
+        InventoryItem toUseCase = createInventoryItemUseCase.createInventoryItem(
+                createInventoryRequestDTO.getProductName(),
+                createInventoryRequestDTO.getAvailableStock(),
+                createInventoryRequestDTO.getUnitPrice()
+        );
+        InventoryResponseDTO responseDTO = inventoryWebMapper.toRecordDTO(toUseCase);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 }
