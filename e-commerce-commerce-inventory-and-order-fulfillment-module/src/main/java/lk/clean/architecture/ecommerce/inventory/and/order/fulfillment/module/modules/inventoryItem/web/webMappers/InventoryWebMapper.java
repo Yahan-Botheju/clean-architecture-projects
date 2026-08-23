28eab87f304337f65d73207fd3e27a630f384d80
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-23 19:53:18 +0530

    feat: initiate inventory web mapper

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/web/webMappers/InventoryWebMapper.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/inventoryItem/web/webMappers/InventoryWebMapper.java
@@ -1,4 +1,12 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.webMappers;
 
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.domain.modules.InventoryItem;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.inventoryItem.web.DTOs.InventoryResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface InventoryWebMapper {
+
+    //domain model to dto
+    InventoryResponseDTO toRecordDTO(InventoryItem inventoryItem);
 }
