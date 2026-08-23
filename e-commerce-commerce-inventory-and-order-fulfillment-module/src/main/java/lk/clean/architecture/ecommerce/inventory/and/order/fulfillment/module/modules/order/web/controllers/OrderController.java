6bb27ba1d8029f8956a4c482ae0fff980732c8a3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-23 20:42:08 +0530

    feat: setup place order endpoint

--- e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/web/controllers/OrderController.java
+++ e-commerce-commerce-inventory-and-order-fulfillment-module/src/main/java/lk/clean/architecture/ecommerce/inventory/and/order/fulfillment/module/modules/order/web/controllers/OrderController.java
@@ -1,9 +1,17 @@
 package lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.web.controllers;
 
+import jakarta.validation.Valid;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.domain.modules.Order;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase.ConfirmOrderUseCase;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase.PlaceOrderUseCase;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.usecase.ShipOrderUseCase;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.web.DTOs.OrderResponseDTO;
+import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.web.DTOs.PlaceOrderRequestDTO;
 import lk.clean.architecture.ecommerce.inventory.and.order.fulfillment.module.modules.order.web.webMappers.OrderWebMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +35,22 @@ public class OrderController {
         this.confirmOrderUseCase = confirmOrderUseCase;
         this.shipOrderUseCase = shipOrderUseCase;
         this.orderWebMapper = orderWebMapper;
+    }
+
+    //place order
+    @PostMapping
+    public ResponseEntity<OrderResponseDTO> placeOrder(
+            @Valid @RequestBody PlaceOrderRequestDTO placeOrderRequestDTO
+    ){
+        //set values to usecase
+        Order toUseCase = placeOrderUseCase.placeOrder(
+                placeOrderRequestDTO.getCustomerId(),
+                placeOrderRequestDTO.getProductId(),
+                placeOrderRequestDTO.getRequestQuantity()
+        );
+
+        OrderResponseDTO responseDTO = orderWebMapper.toResponseDTO(toUseCase);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 }
