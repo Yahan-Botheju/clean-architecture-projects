0a37d488d700d3bc384f0ee5be9d6a8ed4dee32c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-25 23:04:34 +0530

    feat: create book status enum

---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/enums/BookStatus.java
@@ -0,0 +1,6 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.domain.enums;
+
+public enum BookStatus {
+    AVAILABLE,
+    BORROWED
+}
---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/models/Book.java
@@ -0,0 +1,4 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.domain.models;
+
+public class Book {
+}
