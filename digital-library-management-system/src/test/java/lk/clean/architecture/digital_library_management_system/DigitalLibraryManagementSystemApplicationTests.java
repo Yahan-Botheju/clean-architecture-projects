3157e67bc7951beeca9b0ced089324b92393acf2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-25 22:46:15 +0530

    project 03 initial commit

---
+++ digital-library-management-system/src/test/java/lk/clean/architecture/digital_library_management_system/DigitalLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.digital_library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
