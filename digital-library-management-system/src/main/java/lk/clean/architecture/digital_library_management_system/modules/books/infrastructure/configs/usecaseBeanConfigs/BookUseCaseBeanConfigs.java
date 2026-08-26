8b3be054ceddadb134a715caee3aa5d775a76b6f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 14:18:55 +0530

    feat: initiate book use case impl as service class through config class

---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/configs/usecaseBeanConfigs/BookUseCaseBeanConfigs.java
@@ -0,0 +1,19 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.configs.usecaseBeanConfigs;
+
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookUseCase;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BookUseCaseBeanConfigs {
+
+    //book usecase bean config
+    @Bean
+    public BookUseCase bookUseCase(
+            BookRepository bookRepository
+    ) {
+        return new BookUseCaseImpl(bookRepository);
+    }
+}
