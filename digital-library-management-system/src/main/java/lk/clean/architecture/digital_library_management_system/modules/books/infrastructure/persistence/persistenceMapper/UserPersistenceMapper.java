1000e0cfb34f3430ec9fc1c681dbe98236b74f6c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 11:53:49 +0530

    feat: initiate user persistence mapper

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/persistenceMapper/UserPersistenceMapper.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/persistenceMapper/UserPersistenceMapper.java
@@ -1,4 +1,13 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.persistenceMapper;
 
+import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.entities.UserEntity;
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.models.User;
+
 public interface UserPersistenceMapper {
+
+    //domain model to entity
+    UserEntity toEntity(User user);
+
+    //entity to domain model
+    User toDomainModel(UserEntity userEntity);
 }
