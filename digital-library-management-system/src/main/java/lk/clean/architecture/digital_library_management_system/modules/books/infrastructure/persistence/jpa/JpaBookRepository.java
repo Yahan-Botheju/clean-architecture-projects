ab59b07495052e56960e553661a2cba07f18d8a0
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 13:31:59 +0530

    feat: check book existence by book id

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/jpa/JpaBookRepository.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/jpa/JpaBookRepository.java
@@ -3,7 +3,10 @@ package lk.clean.architecture.digital_library_management_system.modules.books.in
 import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaBookRepository extends JpaRepository<BookEntity, UUID> {
+
+    boolean existsById(UUID bookId);
 }
