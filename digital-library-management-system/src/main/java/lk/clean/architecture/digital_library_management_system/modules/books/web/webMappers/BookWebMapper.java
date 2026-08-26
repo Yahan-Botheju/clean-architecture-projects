f1bbb3656495a81b1d9298ff1960791f7ed58140
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 19:09:14 +0530

    feat: update web mapper

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/webMappers/BookWebMapper.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/webMappers/BookWebMapper.java
@@ -1,7 +1,9 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.web.webMappers;
 
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowCommand;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
 import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowRequestDTO;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowedResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +11,7 @@ public interface BookWebMapper {
 
     //to user command
     BookBorrowCommand toBookBorrowCommand(BookBorrowRequestDTO bookBorrowRequestDTO);
+
+    //domain model to responseDTO
+    BookBorrowedResponseDTO toBookBorrowResponseDTO(BookBorrowResult bookBorrowResult);
 }
