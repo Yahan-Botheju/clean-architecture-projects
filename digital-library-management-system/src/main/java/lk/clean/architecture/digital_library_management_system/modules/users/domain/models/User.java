f5c94d49302bcf4cf692675bf949a14fbdfb9b01
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 13:10:43 +0530

    feat: create user domain logic

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/domain/models/User.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/domain/models/User.java
@@ -1,5 +1,7 @@
 package lk.clean.architecture.digital_library_management_system.modules.users.domain.models;
 
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.enums.BookStatus;
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.models.Book;
 import lk.clean.architecture.digital_library_management_system.modules.users.domain.enums.UserStatus;
 
 import java.util.UUID;
@@ -17,9 +19,26 @@ public class User {
         this.status = status;
     }
 
+
     /* __GETTERS__ */
 
     public UUID getUserId() { return userId; }
     public String getUserName() { return userName; }
     public String getEmail() { return email; }
-    public UserStatus getStatus() { return status; }}
+    public UserStatus getStatus() { return status; }
+
+
+    /* __DOMAIN_LOGICS__ */
+
+
+    //suspended user cannot borrow book
+    public void borrowBook(Book book) {
+        if(this.status == UserStatus.SUSPENDED){
+            throw new IllegalStateException("User has been suspended");
+        }
+        book.borrowBook(this.userId);
+    }
+
+
+}
+
