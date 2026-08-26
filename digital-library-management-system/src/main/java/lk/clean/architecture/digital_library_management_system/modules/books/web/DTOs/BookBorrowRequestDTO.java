2e3c5eb6718b4a24a5467a055c37d15086f6d623
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 13:40:02 +0530

    feat: create book request dto

---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/DTOs/BookBorrowRequestDTO.java
@@ -0,0 +1,24 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookBorrowRequestDTO {
+
+    @NotNull(message = "User ID is required")
+    private UUID userId;
+
+    @NotBlank(message = "Book title cannot be empty")
+    private String title;
+
+    @NotBlank(message = "Author name cannot be empty")
+    private String author;
+}
