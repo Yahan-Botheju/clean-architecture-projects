b03faf3da95a0583d0d6f9ad44a7a01d1946c2d9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 13:17:58 +0530

    feat: inject required depndencies

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/BookPersistenceImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/BookPersistenceImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence;
 
-public class BookPersistenceImpl {
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
+import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.jpa.JpaBookRepository;
+import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.persistenceMapper.BookPersistenceMapper;
+
+public class BookPersistenceImpl implements BookRepository {
+
+    //inject required dependencies
+    private final JpaBookRepository jpaBookRepository;
+    private final BookPersistenceMapper bookPersistenceMapper;
+
+    public BookPersistenceImpl(
+            JpaBookRepository jpaBookRepository,
+            BookPersistenceMapper bookPersistenceMapper
+    ) {
+        this.jpaBookRepository = jpaBookRepository;
+        this.bookPersistenceMapper = bookPersistenceMapper;
+    }
 }
