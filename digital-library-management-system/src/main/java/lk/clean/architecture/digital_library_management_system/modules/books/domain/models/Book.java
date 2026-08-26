d803a6be78b27d38635a8bd77f284bac27fe3d18
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 13:10:11 +0530

    feat: create domain logics for check book availability and add a user id field

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/models/Book.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/models/Book.java
@@ -1,18 +1,21 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.domain.models;
 
 import lk.clean.architecture.digital_library_management_system.modules.books.domain.enums.BookStatus;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
 
 public class Book {
     private final UUID bookId;
+    private UUID borrowedByUserId;
     private String isbn;
     private String title;
     private String author;
     private BookStatus status;
 
-    public Book(UUID bookId, String isbn, String title, String author, BookStatus status) {
+    public Book(UUID bookId, UUID borrowedByUserId, String isbn, String title, String author, BookStatus status) {
         this.bookId = bookId;
+        this.borrowedByUserId = borrowedByUserId;
         this.isbn = isbn;
         this.title = title;
         this.author = author;
@@ -22,9 +25,23 @@ public class Book {
     /* __GETTER__ */
 
     public UUID getBookId() { return bookId; }
+    public UUID getUserId() { return borrowedByUserId; }
     public String getIsbn() { return isbn; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public BookStatus getStatus() { return status; }
+
+
+    /* __DOMAIN_LOGICS__ */
+
+
+    //check book status
+    public void borrowBook(UUID userId) {
+        if(this.status != BookStatus.AVAILABLE) {
+            throw new ResourceNotFoundException("Book status is not AVAILABLE");
+        }
+        this.status = BookStatus.BORROWED;
+        this.borrowedByUserId = userId;
+    }
 
 }
