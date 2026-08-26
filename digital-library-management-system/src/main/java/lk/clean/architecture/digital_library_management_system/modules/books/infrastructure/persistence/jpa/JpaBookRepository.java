34cf52785676bd6c1f1480f7338e8fd14f05bc78
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 12:12:45 +0530

    feat: create book entity

---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/jpa/JpaBookRepository.java
@@ -0,0 +1,4 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.jpa;
+
+public interface JpaBookRepository {
+}
---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/persistenceMapper/BookPersistenceMapper.java
@@ -0,0 +1,8 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.persistenceMapper;
+
+import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.entities.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookPersistenceMapper
