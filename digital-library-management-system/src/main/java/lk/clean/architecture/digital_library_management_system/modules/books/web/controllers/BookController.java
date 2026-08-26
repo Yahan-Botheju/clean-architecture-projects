01de04c7f99ef402f7758cc9492a42901c981b69
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-26 19:51:48 +0530

    feat: steup book borrow endpoint

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/controllers/BookController.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/controllers/BookController.java
@@ -1,7 +1,16 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.web.controllers;
 
+import jakarta.validation.Valid;
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.models.Book;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookUseCase;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowCommand;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowRequestDTO;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowedResponseDTO;
 import lk.clean.architecture.digital_library_management_system.modules.books.web.webMappers.BookWebMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +25,20 @@ public class BookController {
     public BookController(BookUseCase bookUseCase, BookWebMapper bookWebMapper) {
         this.bookUseCase = bookUseCase;
         this.bookWebMapper = bookWebMapper;
+    }
+
+    //borrow a book
+    @PostMapping
+    public ResponseEntity<BookBorrowedResponseDTO>  borrowBook(
+            @Valid BookBorrowRequestDTO bookBorrowRequestDTO
+    ){
+        //dto -> command
+        BookBorrowCommand toBorrowCommand = bookWebMapper.toBookBorrowCommand(bookBorrowRequestDTO);
+        //command -> usecase
+        BookBorrowResult toUseCase = bookUseCase.bookBorrow(toBorrowCommand);
+        //command -> response
+        BookBorrowedResponseDTO responseDTO = bookWebMapper.toBookBorrowResponseDTO(toUseCase);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
     }
 }
