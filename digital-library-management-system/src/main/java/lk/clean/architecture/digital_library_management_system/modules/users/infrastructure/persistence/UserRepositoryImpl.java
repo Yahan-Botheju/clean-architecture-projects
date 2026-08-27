dc3e0229ceb7efebaedfb58bc6ed5fa4c7d9e9c1
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:27:17 +0530

    feat: get user by id from db

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/UserRepositoryImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/UserRepositoryImpl.java
@@ -1,8 +1,12 @@
 package lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence;
 
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.models.User;
 import lk.clean.architecture.digital_library_management_system.modules.users.domain.repositories.UserRepository;
 import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.jpa.JpaUserRepository;
 import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.persistenceMapper.UserPersistenceMapper;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class UserRepositoryImpl implements UserRepository {
 
@@ -16,5 +20,12 @@ public class UserRepositoryImpl implements UserRepository {
     ) {
         this.jpaUserRepository = jpaUserRepository;
         this.userPersistenceMapper = userPersistenceMapper;
+    }
+
+    //get user by id
+    @Override
+    public Optional<User> getUserById(UUID userId) {
+        return jpaUserRepository.findById(userId)
+                .map(userPersistenceMapper::toDomainModel);
     }
 }
