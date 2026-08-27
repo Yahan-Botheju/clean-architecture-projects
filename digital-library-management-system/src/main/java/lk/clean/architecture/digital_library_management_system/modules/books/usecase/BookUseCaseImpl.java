65df52b739c60780612f7170e30bd1426bdc406a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:58:26 +0530

    feat: inject user details api for user check from user domain

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookUseCaseImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookUseCaseImpl.java
@@ -1,4 +1,25 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.usecase;
 
-public class BookUseCaseImpl {
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowCommand;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.api.UserDetailsApi;
+
+public class BookUseCaseImpl implements BookUseCase {
+
+    //inject required dependencies
+    private final BookRepository bookRepository;
+    private final UserDetailsApi userDetailsApi;
+
+    public BookUseCaseImpl(BookRepository bookRepository, UserDetailsApi userDetailsApi) {
+        this.bookRepository = bookRepository;
+        this.userDetailsApi = userDetailsApi;
+    }
+
+    @Override
+    public BookBorrowResult bookBorrow(BookBorrowCommand bookBorrowCommand){
+
+
+            return null;
+    }
 }
