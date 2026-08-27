ee92ecaaef8675aca551c4efe70dae8796f5ee75
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 19:57:06 +0530

    feat: initiate method

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookUseCase.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookUseCase.java
@@ -1,4 +1,10 @@
-package lk.clean.architecture.digital_library_management_system.modules.books.usecase.records;
+package lk.clean.architecture.digital_library_management_system.modules.books.usecase;
+
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowCommand;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
 
 public interface BookUseCase {
+
+    //borrow book
+    BookBorrowResult bookBorrow(BookBorrowCommand bookBorrowCommand);
 }
