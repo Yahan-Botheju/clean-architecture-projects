44e0dd10e8a59a451f4adfd791d6cae4e2b2c595
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:26:57 +0530

    feat: find user by id custom method

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/jpa/JpaUserRepository.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/jpa/JpaUserRepository.java
@@ -3,7 +3,11 @@ package lk.clean.architecture.digital_library_management_system.modules.users.in
 import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    //user find by id
+    Optional<UserEntity> findById(UUID userId);
 }
