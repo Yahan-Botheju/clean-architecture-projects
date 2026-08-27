ee579f855696b9eca87900f1f44adf780d2bf3bb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 20:08:10 +0530

    feat: initiate method

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/repositories/BookRepository.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/repositories/BookRepository.java
@@ -1,10 +1,15 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories;
 
 
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.models.Book;
+
 import java.util.UUID;
 
 public interface BookRepository {
 
     //check book existence
     boolean existsById(UUID bookId);
+
+    //get book by id
+    Book getBookById(UUID bookId);
 }
