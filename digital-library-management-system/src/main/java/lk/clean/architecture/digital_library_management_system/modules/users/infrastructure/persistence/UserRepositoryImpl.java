3288afc871342b340ce3ced4a72cd90d9c5c6a33
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:23:51 +0530

    feat: inject required dependencies

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/UserRepositoryImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/UserRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence;
 
-public class UserRepositoryImpl {
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.repositories.UserRepository;
+import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.jpa.JpaUserRepository;
+import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.persistenceMapper.UserPersistenceMapper;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    //inject required dependencies
+    private final JpaUserRepository jpaUserRepository;
+    private final UserPersistenceMapper userPersistenceMapper;
+
+    public UserRepositoryImpl(
+            JpaUserRepository jpaUserRepository,
+            UserPersistenceMapper userPersistenceMapper
+    ) {
+        this.jpaUserRepository = jpaUserRepository;
+        this.userPersistenceMapper = userPersistenceMapper;
+    }
 }
