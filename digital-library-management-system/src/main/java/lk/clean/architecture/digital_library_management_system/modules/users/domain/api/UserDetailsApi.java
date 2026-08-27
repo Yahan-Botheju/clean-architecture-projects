b7ca4bbec69860d9fa0e8efa295a0d1f75177e0f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:55:12 +0530

    feat: initiate method

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/domain/api/UserDetailsApi.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/domain/api/UserDetailsApi.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.digital_library_management_system.modules.users.domain.api;
 
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.models.User;
+
+import java.util.UUID;
+
 public interface UserDetailsApi {
+
+    //user details for external domain usage
+    User sharedUserDetails(UUID userId);
 }
