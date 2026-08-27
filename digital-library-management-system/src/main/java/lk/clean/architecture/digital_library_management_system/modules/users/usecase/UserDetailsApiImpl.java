f64ea128c5bbf91fb1696adbfc165c1e75b7ed22
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-27 12:56:07 +0530

    feat: initiate functionality of user check and save

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/usecase/UserDetailsApiImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/usecase/UserDetailsApiImpl.java
@@ -1,7 +1,11 @@
 package lk.clean.architecture.digital_library_management_system.modules.users.usecase;
 
 import lk.clean.architecture.digital_library_management_system.modules.users.domain.api.UserDetailsApi;
+import lk.clean.architecture.digital_library_management_system.modules.users.domain.models.User;
 import lk.clean.architecture.digital_library_management_system.modules.users.domain.repositories.UserRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class UserDetailsApiImpl implements UserDetailsApi {
 
@@ -10,5 +14,15 @@ public class UserDetailsApiImpl implements UserDetailsApi {
 
     public UserDetailsApiImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+
+    //user for other domain usage
+    @Override
+    public User sharedUserDetails(UUID userId) {
+        User availableUser = userRepository.getUserById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        return userRepository.save(availableUser);
     }
 }
