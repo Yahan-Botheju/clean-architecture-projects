7a8fa8376bf0854a3ff34d745b97f73a465c3edc
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-28 20:49:24 +0530

    feat: create custom query for user active book count

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/jpa/JpaUserRepository.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/users/infrastructure/persistence/jpa/JpaUserRepository.java
@@ -2,6 +2,8 @@ package lk.clean.architecture.digital_library_management_system.modules.users.in
 
 import lk.clean.architecture.digital_library_management_system.modules.users.infrastructure.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +12,12 @@ public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
 
     //user find by id
     Optional<UserEntity> findById(UUID userId);
+
+    @Query(
+            "SELECT COUNT(b) " +
+                    "FROM BookEntity b " +
+                    "WHERE b.borrowedByUser.userId = :userId " +
+                    "AND b.status = 'BORROWED'"
+    )
+    int countActiveBooksByUserId(@Param("userId") UUID userId);
 }
