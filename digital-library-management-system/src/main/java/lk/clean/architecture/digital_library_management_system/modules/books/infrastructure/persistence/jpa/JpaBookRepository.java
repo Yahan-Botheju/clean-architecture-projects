3e77f68fab3c4c28713bfbadaba42d11e7f20875
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-29 12:16:55 +0530

    feat: create custom query to check user has already borrow a book that has same book currently going to buy

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/jpa/JpaBookRepository.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/persistence/jpa/JpaBookRepository.java
@@ -2,11 +2,23 @@ package lk.clean.architecture.digital_library_management_system.modules.books.in
 
 import lk.clean.architecture.digital_library_management_system.modules.books.infrastructure.persistence.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaBookRepository extends JpaRepository<BookEntity, UUID> {
 
     boolean existsById(UUID bookId);
+
+    //create custom query to check same books are already in user entity
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END " +
+            "FROM BookEntity b " +
+            "WHERE b.borrowedByUserId = :userId " +
+            "AND LOWER(b.title) = LOWER(:title) " +
+            "AND b.status = 'BORROWED'")
+    boolean checkUserBorrowedBooks(
+            @Param("userId") UUID userId,
+            @Param("title") String title
+    );
 }
