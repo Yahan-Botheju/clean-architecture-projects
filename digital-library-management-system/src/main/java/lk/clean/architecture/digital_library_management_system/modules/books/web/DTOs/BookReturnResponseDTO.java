4bb17eab3dd6f0220326a49c0ebe2250fdf002d8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-29 19:53:21 +0530

    feat: initiate book return response dto

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/DTOs/BookReturnResponseDTO.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/DTOs/BookReturnResponseDTO.java
@@ -1,4 +1,19 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookReturnResponseDTO {
+    private UUID bookId;
+    private String title;
+    private UUID userId;
+    private String userName;
+    private String email;
+    private String isbn;
 }
