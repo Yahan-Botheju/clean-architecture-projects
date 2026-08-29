7cc1b019f7d81a9684a52d99ae32adfc8b892114
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-29 12:33:17 +0530

    feat: add new fields

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/records/BookBorrowResult.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/records/BookBorrowResult.java
@@ -1,5 +1,8 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.usecase.records;
 
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.enums.BookStatus;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookBorrowResult(
@@ -7,5 +10,8 @@ public record BookBorrowResult(
         UUID borrowedByUserId,
         String isbn,
         String title,
-        String author
+        String author,
+        LocalDate borrowedDate,
+        LocalDate dueDate,
+        BookStatus bookStatus
 ) {}
