4dc1969878bc9baf5235816a08cfb1079fb0db54
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-29 19:39:34 +0530

    update: add new borrow status field to domain

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/models/Book.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/domain/models/Book.java
@@ -1,6 +1,7 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.domain.models;
 
 import lk.clean.architecture.digital_library_management_system.modules.books.domain.enums.BookStatus;
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.enums.BorrowStatus;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
@@ -12,14 +13,16 @@ public class Book {
     private String title;
     private String author;
     private BookStatus status;
+    private BorrowStatus  borrowStatus;
 
-    public Book(UUID bookId, UUID borrowedByUserId, String isbn, String title, String author, BookStatus status) {
+    public Book(UUID bookId, UUID borrowedByUserId, String isbn, String title, String author, BookStatus status, BorrowStatus borrowStatus) {
         this.bookId = bookId;
         this.borrowedByUserId = borrowedByUserId;
         this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.status = status;
+        this.borrowStatus = borrowStatus;
     }
 
     /* __GETTER__ */
@@ -30,6 +33,7 @@ public class Book {
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public BookStatus getStatus() { return status; }
+    public BorrowStatus getBorrowStatus() { return borrowStatus; }
 
 
     /* __DOMAIN_LOGICS__ */
@@ -41,6 +45,7 @@ public class Book {
             throw new ResourceNotFoundException("Book status is not AVAILABLE");
         }
         this.status = BookStatus.BORROWED;
+        this.borrowStatus = BorrowStatus.ACTIVE;
         this.borrowedByUserId = userId;
     }
 
