6a84f0f8f141b59cdc77b91d2b260c2063956d80
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-30 18:52:25 +0530

    feat: inject required dependencies

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/controllers/BookController.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/controllers/BookController.java
@@ -2,16 +2,19 @@ package lk.clean.architecture.digital_library_management_system.modules.books.we
 
 import jakarta.validation.Valid;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookBorrowUseCase;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookReturnUseCase;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowCommand;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookReturnCommand;
 import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowRequestDTO;
 import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookBorrowedResponseDTO;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookReturnRequestDTO;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookReturnResponseDTO;
 import lk.clean.architecture.digital_library_management_system.modules.books.web.webMappers.BookBorrowWebMapper;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.webMappers.BookReturnWebMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,15 +22,25 @@ public class BookController {
 
     //inject required dependencies
     private final BookBorrowUseCase bookBorrowUseCase;
+    private final BookReturnUseCase bookReturnUseCase;
     private final BookBorrowWebMapper bookBorrowWebMapper;
+    private final BookReturnWebMapper bookReturnWebMapper;
 
-    public BookController(BookBorrowUseCase bookBorrowUseCase, BookBorrowWebMapper bookBorrowWebMapper) {
+
+    public BookController(
+            BookBorrowUseCase bookBorrowUseCase,
+            BookReturnUseCase bookReturnUseCase,
+            BookBorrowWebMapper bookBorrowWebMapper,
+            BookReturnWebMapper bookReturnWebMapper
+    ) {
         this.bookBorrowUseCase = bookBorrowUseCase;
+        this.bookReturnUseCase = bookReturnUseCase;
         this.bookBorrowWebMapper = bookBorrowWebMapper;
+        this.bookReturnWebMapper = bookReturnWebMapper;
     }
 
-    //borrow a book
-    @PostMapping
+    //borrow a book endpoint
+    @PostMapping("/borrowings")
     public ResponseEntity<BookBorrowedResponseDTO>  borrowBook(
             @Valid BookBorrowRequestDTO bookBorrowRequestDTO
     ){
@@ -38,6 +51,14 @@ public class BookController {
         //command -> response
         BookBorrowedResponseDTO responseDTO = bookBorrowWebMapper.toBookBorrowResponseDTO(toUseCase);
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    //book return endpoint
+    @PostMapping("/borrowings/return")
+    public ResponseEntity<BookReturnResponseDTO>  returnBook(
+            @Valid @RequestBody BookReturnRequestDTO bookReturnRequestDTO
+    ){
+        BookReturnCommand toReturnCommand =
     }
 }
