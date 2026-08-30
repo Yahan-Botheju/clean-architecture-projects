b1690c42213c3dacdc691f6963ebe99d20eeda87
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-30 18:46:33 +0530

    feat: initiate book return web mapper

---
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/web/webMappers/BookReturnWebMapper.java
@@ -0,0 +1,17 @@
+package lk.clean.architecture.digital_library_management_system.modules.books.web.webMappers;
+
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookBorrowResult;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookReturnCommand;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookReturnRequestDTO;
+import lk.clean.architecture.digital_library_management_system.modules.books.web.DTOs.BookReturnResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface BookReturnWebMapper {
+
+    //requestDTO to command
+    BookReturnCommand toBookReturnCommand(BookReturnRequestDTO bookReturnRequestDTO);
+
+    //domain model to responseDTO
+    BookReturnResponseDTO toBookReturnResponseDTO(BookBorrowResult bookBorrowResult);
+}
