344bb2147d61b2ee2b43278e8c4f7270851bc622
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-30 19:17:50 +0530

    feat: initiate book return usecase impl as service class through config class

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/configs/usecaseBeanConfigs/BookUseCaseBeanConfigs.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/infrastructure/configs/usecaseBeanConfigs/BookUseCaseBeanConfigs.java
@@ -3,6 +3,8 @@ package lk.clean.architecture.digital_library_management_system.modules.books.in
 import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookBorrowUseCase;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookBorrowUseCaseImpl;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookReturnUseCase;
+import lk.clean.architecture.digital_library_management_system.modules.books.usecase.BookReturnUseCaseImpl;
 import lk.clean.architecture.digital_library_management_system.modules.users.domain.api.UserDetailsApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BookUseCaseBeanConfigs {
 
-    //book usecase bean config
+    //book borrow usecase impl
     @Bean
     public BookBorrowUseCase bookUseCase(
             BookRepository bookRepository,
             UserDetailsApi userDetailsApi
     ) {
         return new BookBorrowUseCaseImpl(bookRepository, userDetailsApi);
+    }
+
+    //book return usecase impl
+    @Bean
+    public BookReturnUseCase bookReturnUseCase(
+            BookRepository bookRepository
+    ){
+        return new BookReturnUseCaseImpl(bookRepository);
     }
 }
