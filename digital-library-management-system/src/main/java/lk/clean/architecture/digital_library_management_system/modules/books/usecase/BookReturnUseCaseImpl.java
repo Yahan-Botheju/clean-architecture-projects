0ce50a93c370ab4790f0712d91a77275e21f2430
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-30 19:18:10 +0530

    feat: inject required dependencies

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookReturnUseCaseImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookReturnUseCaseImpl.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.usecase;
 
-public class BookReturnUseCaseImpl {
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
+
+public class BookReturnUseCaseImpl implements BookReturnUseCase {
+
+    //inject required dependencies
+    private final BookRepository bookRepository;
+
+    public BookReturnUseCaseImpl(BookRepository bookRepository) {
+        this.bookRepository=bookRepository;
+    }
+
 }
