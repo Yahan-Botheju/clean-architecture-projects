2f7e2bdd09f1d78d238a283aa4ad889e5a2a6284
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 13:24:46 +0530

    feat: create book return usecase impl

--- digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookReturnUseCaseImpl.java
+++ digital-library-management-system/src/main/java/lk/clean/architecture/digital_library_management_system/modules/books/usecase/BookReturnUseCaseImpl.java
@@ -1,5 +1,6 @@
 package lk.clean.architecture.digital_library_management_system.modules.books.usecase;
 
+import lk.clean.architecture.digital_library_management_system.modules.books.domain.models.Book;
 import lk.clean.architecture.digital_library_management_system.modules.books.domain.repositories.BookRepository;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookReturnCommand;
 import lk.clean.architecture.digital_library_management_system.modules.books.usecase.records.BookReturnResult;
@@ -17,7 +18,27 @@ public class BookReturnUseCaseImpl implements BookReturnUseCase {
     @Override
     public BookReturnResult returnBook(BookReturnCommand bookReturnCommand) {
 
+        //find active borrow record
+        Book activeBook = bookRepository.findBorrowedBookRecord(bookReturnCommand.userId(), bookReturnCommand.bookId());
 
+        /*
+        * domain logic =>
+        * book borrow status returned
+        * book status borrowed -> available
+        *
+        */
+        activeBook.returnBook();
 
+        //update book
+        Book returendBook = bookRepository.updateBookReturn(activeBook);
+
+        return new BookReturnResult(
+                returendBook.getBookId(),
+                returendBook.getTitle(),
+                returendBook.getUserId(),
+                returendBook.getIsbn(),
+                returendBook.getStatus(),
+                returendBook.getBorrowStatus()
+        );
     }
 }
