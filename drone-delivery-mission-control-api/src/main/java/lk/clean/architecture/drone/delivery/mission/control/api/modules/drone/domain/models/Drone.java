06c374b30e2d589b4438412d204fcf952f18d978
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 20:21:37 +0530

    feat: create max payload check logic

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/domain/models/Drone.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/domain/models/Drone.java
@@ -5,26 +5,38 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.do
 import java.util.UUID;
 
 public class Drone {
+
+    static final double maxPayloadingKg = 0.5;
+
     private UUID droneId;
     private String serialNumber;
     private DroneStatus droneStatus;
-    private double maxPayloadingKg;
+    private double payloadingKg;
     private double batteryPercentage;
 
-    public Drone(UUID droneId, String serialNumber, DroneStatus droneStatus, double maxPayloadingKg, double batteryPercentage) {
+    public Drone(UUID droneId, String serialNumber, DroneStatus droneStatus, double payloadingKg,double batteryPercentage) {
         this.droneId = droneId;
         this.serialNumber = serialNumber;
         this.droneStatus = droneStatus;
-        this.maxPayloadingKg = maxPayloadingKg;
+        this.payloadingKg = payloadingKg;
         this.batteryPercentage = batteryPercentage;
     }
 
     public UUID getDroneId() { return droneId; }
     public String getSerialNumber() { return serialNumber; }
     public DroneStatus getDroneStatus() { return droneStatus; }
-    public double getMaxPayloadingKg() { return maxPayloadingKg; }
+    public double getPayloadingKg() { return payloadingKg; }
     public double getBatteryPercentage() { return batteryPercentage; }
 
 
 
+    /* __DOMAIN_LOGIC__ */
+
+
+    //check delivery pay load
+    public void checkDronePayload(){
+        if(this.payloadingKg >= maxPayloadingKg){
+            throw new IllegalStateException("Pay loading KG exceeds maximum allowed");
+        }
+    }
 }
