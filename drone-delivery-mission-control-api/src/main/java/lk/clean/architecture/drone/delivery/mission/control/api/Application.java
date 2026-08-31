55337df877c75e6fb4f7d89581cffa5aefb0e6b3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 19:11:18 +0530

    project 04 initial commit

---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/Application.java
@@ -0,0 +1,13 @@
+package lk.clean.architecture.drone.delivery.mission.control.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
