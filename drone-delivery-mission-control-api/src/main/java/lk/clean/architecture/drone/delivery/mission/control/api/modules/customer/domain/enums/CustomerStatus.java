14276f4456c8fe48e2487153c82cf39c281dd51c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 19:29:57 +0530

    feat: crete customer enum

---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/domain/enums/CustomerStatus.java
@@ -0,0 +1,6 @@
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.enums;
+
+public enum CustomerStatus {
+    ACTIVE,
+    SUSPENDED,
+}
---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/entities/CustomerEntity.java
@@ -0,0 +1,4 @@
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.entities;
+
+public class CustomerEntity {
+}
