740d6b164b219917ba7189e017fc7122057b78fd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 19:59:09 +0530

    feat: create customer persistence mapper

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/persistenceMappers/CustomerPersistenceMapper.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/persistenceMappers/CustomerPersistenceMapper.java
@@ -1,4 +1,15 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.persistenceMappers;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.models.Customer;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.entities.CustomerEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CustomerPersistenceMapper {
+
+    //domain model to entity
+    CustomerEntity toEntity(Customer customer);
+
+    //entity to domain model
+    Customer toDomainModel(CustomerEntity customerEntity);
 }
