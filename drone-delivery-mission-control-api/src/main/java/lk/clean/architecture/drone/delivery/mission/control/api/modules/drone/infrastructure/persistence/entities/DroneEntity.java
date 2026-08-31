bb525950b09cca3bf274f0f00c04423b210e205c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 20:32:15 +0530

    feat: create drone entity

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/persistence/entities/DroneEntity.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/persistence/entities/DroneEntity.java
@@ -1,4 +1,34 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.persistence.entities;
 
+import jakarta.persistence.*;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums.DroneStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "drones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DroneEntity {
+
+    @Id
+    @Column(name = "drone_id", nullable = false)
+    private UUID droneId;
+
+    @Column(name = "serial_number", nullable = false, unique = true)
+    private String serialNumber;
+
+    @Column(name = "serial_number", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DroneStatus droneStatus;
+
+    @Column(name = "pay_loading_KG")
+    private double payloadingKg;
+
+    @Column(name = "battery_percentage", nullable = false)
+    private double batteryPercentage;
 }
