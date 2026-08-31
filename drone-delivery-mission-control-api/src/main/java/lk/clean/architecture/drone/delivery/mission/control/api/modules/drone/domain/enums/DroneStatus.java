a482d6086c613d1b4be66500e27f190fb12be96f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-08-31 20:07:57 +0530

    feat: create drone status enum

---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/domain/enums/DroneStatus.java
@@ -0,0 +1,8 @@
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums;
+
+public enum DroneStatus {
+    AVAILABLE,
+    ON_MISSION,
+    MAINTENANCE,
+    OFFLINE
+}
