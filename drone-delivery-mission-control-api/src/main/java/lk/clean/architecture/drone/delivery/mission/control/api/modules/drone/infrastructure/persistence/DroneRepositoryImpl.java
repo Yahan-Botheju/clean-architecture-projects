98473a8cc7a246533eaf2bf737b61737fe0cddbd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-01 19:28:03 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/persistence/DroneRepositoryImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/persistence/DroneRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.persistence;
 
-public class DroneRepositoryImpl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.persistence.jpa.JpaDroneRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.persistence.persistenceMappers.DronePersistenceMapper;
+
+public class DroneRepositoryImpl implements DroneRepository {
+
+    //inject required dependencies
+    private final JpaDroneRepository jpaDroneRepository;
+    private final DronePersistenceMapper dronePersistenceMapper;
+
+    public DroneRepositoryImpl(
+            JpaDroneRepository jpaDroneRepository,
+            DronePersistenceMapper dronePersistenceMapper
+    ) {
+        this.jpaDroneRepository = jpaDroneRepository;
+        this.dronePersistenceMapper = dronePersistenceMapper;
+    }
 }
