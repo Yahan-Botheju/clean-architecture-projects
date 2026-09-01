a210459c8152006bf2e1edb79b4f34d96369b414
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-01 11:32:45 +0530

    feat: add validation

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/DeliveryRequestDTO.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/DeliveryRequestDTO.java
@@ -1,5 +1,7 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,15 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DeliveryRequestDTO {
+    @NotNull(message = "Customer ID cannot be empty")
     private UUID customerId;
+
+    @NotNull(message = "Package weight cannot be empty")
     private double packageWeightKg;
+
+    @NotBlank(message = "Pick up location cannot be empty")
     private String pickupLocation;
+
+    @NotBlank(message = "Delivery location cannot be empty")
     private String deliveryLocation;
 }
