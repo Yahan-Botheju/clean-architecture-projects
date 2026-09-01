8a9c9f272bc457ad9a5c1cd32517f93afd3cd14a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-01 21:22:39 +0530

    feat: create functinality of drone task complete impl class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskCompleteApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskCompleteApiImpl.java
@@ -1,7 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskCompleteApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class DroneTaskCompleteApiImpl implements DroneTaskCompleteApi {
 
@@ -10,5 +14,19 @@ public class DroneTaskCompleteApiImpl implements DroneTaskCompleteApi {
 
     public DroneTaskCompleteApiImpl(DroneRepository droneRepository) {
         this.droneRepository=droneRepository;
+    }
+
+    //When drone delivery task is complete
+    @Override
+    public void droneTaskComplete(UUID droneId){
+
+        //check drone existence
+        Drone getDrone = droneRepository.droneFindById(droneId)
+                .orElseThrow(() -> new ResourceNotFoundException("Drone not found!"));
+
+        //call domain method
+        getDrone.markAsCompleteDroneMission();
+
+        droneRepository.saveDrone(getDrone);
     }
 }
