a75a1a9517c514c6644baa9f641cfe601df0fac6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-01 13:04:11 +0530

    feat: initiate drone assign api impl as service class through config class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,19 @@
-package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.configs;
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.configs;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.DroneAssignApiImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    //externally usage api
+    @Bean
+    public DroneAssignApi droneAssignApi(
+            DroneRepository droneRepository
+    ) {
+        return new DroneAssignApiImpl(droneRepository);
+    }
 }
