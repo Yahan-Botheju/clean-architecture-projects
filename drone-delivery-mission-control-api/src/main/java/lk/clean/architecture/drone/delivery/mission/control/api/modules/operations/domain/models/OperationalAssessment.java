61162c4df3b8c37fd29aa44c6e7d8855c80a9d5e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 22:04:09 +0530

    feat: create rule number 1

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/domain/models/OperationalAssessment.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/domain/models/OperationalAssessment.java
@@ -8,4 +8,15 @@ public record OperationalAssessment(
         AirSpaceStatus airSpaceStatus,
         boolean allowed,
         String reason
-) {}
+) {
+
+    //check weather and air is okay to fly the drone
+    public void checkFlightSafety(){
+        if(this.weatherCondition == WeatherCondition.STORM
+                && (this.airSpaceStatus == AirSpaceStatus.CLOSED
+                || this.airSpaceStatus == AirSpaceStatus.RESTRICTED)
+        ){
+            throw new IllegalArgumentException("Cannot continue mission under these condition");
+        }
+    }
+}
