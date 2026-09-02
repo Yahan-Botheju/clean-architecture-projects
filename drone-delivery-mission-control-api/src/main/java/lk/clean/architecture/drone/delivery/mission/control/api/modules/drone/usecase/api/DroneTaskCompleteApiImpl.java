5c208b5b3f6b09495c24f6f3ea914462a2d045b9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 11:08:14 +0530

    ref: initiate abstract class and extends it

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskCompleteApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskCompleteApiImpl.java
@@ -3,26 +3,21 @@ package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.u
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskCompleteApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
 
-public class DroneTaskCompleteApiImpl implements DroneTaskCompleteApi {
+public class DroneTaskCompleteApiImpl extends AbstractDroneFindSupport implements DroneTaskCompleteApi {
 
-    //inject required dependencies
-    private DroneRepository droneRepository;
-
+    //initiate abstract class through the method
     public DroneTaskCompleteApiImpl(DroneRepository droneRepository) {
-        this.droneRepository=droneRepository;
+        super(droneRepository);
     }
 
     //When drone delivery task is complete
     @Override
     public void droneTaskComplete(UUID droneId){
 
-        //check drone existence
-        Drone getDrone = droneRepository.droneFindById(droneId)
-                .orElseThrow(() -> new ResourceNotFoundException("Drone not found!"));
+
 
         //call domain method
         getDrone.markAsCompleteDroneMission();
