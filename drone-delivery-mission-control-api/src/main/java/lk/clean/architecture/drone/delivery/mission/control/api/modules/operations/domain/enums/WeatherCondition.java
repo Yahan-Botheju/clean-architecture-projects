5fab6d018cd09d5cc3e3ea008a6ede9fe13002cb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 21:05:57 +0530

    feat: create operation domain enum

---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/domain/enums/WeatherCondition.java
@@ -0,0 +1,8 @@
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums;
+
+public enum WeatherCondition {
+    CLEAR,
+    CLOUDY,
+    RAINY,
+    STORM
+}
