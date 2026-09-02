4f1bc2ab9644244709df60e2c2e32508fda0fc88
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 10:38:15 +0530

    feat: create functionality

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskFailApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskFailApiImpl.java
@@ -1,7 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskFailApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class DroneTaskFailApiImpl implements DroneTaskFailApi {
 
@@ -10,5 +14,16 @@ public class DroneTaskFailApiImpl implements DroneTaskFailApi {
 
     public DroneTaskFailApiImpl(DroneRepository droneRepository) {
         this.droneRepository = droneRepository;
+    }
+
+    @Override
+    public void droneTaskFail(UUID droneId) {
+        //check drone existence
+        Drone checkDrone = droneRepository.droneFindById(droneId)
+                .orElseThrow(() -> new ResourceNotFoundException("Drone not found!"));
+        //call domain logic
+        checkDrone.markAsFailedDroneMission();
+        //save drone activity
+        droneRepository.saveDrone(checkDrone);
     }
 }
