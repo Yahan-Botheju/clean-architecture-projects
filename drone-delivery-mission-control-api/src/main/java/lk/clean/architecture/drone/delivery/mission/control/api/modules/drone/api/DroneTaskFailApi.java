d5c22d39b43c77ebf17fd50ae5df199f754a8000
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 10:37:42 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneTaskFailApi.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneTaskFailApi.java
@@ -1,4 +1,9 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api;
 
+import java.util.UUID;
+
 public interface DroneTaskFailApi {
+
+    //drone task fail
+    public void droneTaskFail(UUID droneId);
 }
