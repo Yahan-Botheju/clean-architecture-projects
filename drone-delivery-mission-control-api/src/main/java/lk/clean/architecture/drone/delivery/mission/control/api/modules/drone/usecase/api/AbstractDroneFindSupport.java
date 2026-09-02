019fc302b95210a23f7f37f12cc60f2f851bd770
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 11:06:27 +0530

    feat: create abstract class for prevent the drone finding process and inject required dependencies

---
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/AbstractDroneFindSupport.java
@@ -0,0 +1,19 @@
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
+
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
+
+public abstract class AbstractDroneFindSupport {
+
+    //inject required dependencies
+    protected final DroneRepository droneRepository;
+
+    protected AbstractDroneFindSupport(DroneRepository droneRepository) {
+        this.droneRepository = droneRepository;
+    }
+
+
+}
