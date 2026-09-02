f3fc9e1cd442013de115368ebbf4b58ef31fb1ef
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-02 11:12:24 +0530

    ref: extende with abstract class and use it inside the method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskFailApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneTaskFailApiImpl.java
@@ -3,24 +3,20 @@ package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.u
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskFailApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
 
-public class DroneTaskFailApiImpl implements DroneTaskFailApi {
+public class DroneTaskFailApiImpl extends AbstractDroneFindSupport implements DroneTaskFailApi {
 
-    //inject required dependencies
-    private final DroneRepository droneRepository;
-
+    //initiate abstract class
     public DroneTaskFailApiImpl(DroneRepository droneRepository) {
-        this.droneRepository = droneRepository;
+        super(droneRepository);
     }
 
     @Override
     public void droneTaskFail(UUID droneId) {
         //check drone existence
-        Drone checkDrone = droneRepository.droneFindById(droneId)
-                .orElseThrow(() -> new ResourceNotFoundException("Drone not found!"));
+        Drone checkDrone = getDroneById(droneId);
         //call domain logic
         checkDrone.markAsFailedDroneMission();
         //save drone activity
