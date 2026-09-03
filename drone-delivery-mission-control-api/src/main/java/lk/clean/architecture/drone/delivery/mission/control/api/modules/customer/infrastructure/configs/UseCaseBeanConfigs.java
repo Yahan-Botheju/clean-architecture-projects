5312500687b3bf1adc68cf13682aebb66dacf370
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 20:44:13 +0530

    feat: initiate customer check api impl as service class through config class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,17 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.configs;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.api.CustomerCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.repositories.CustomerRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.usecase.api.CustomerCheckApiImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    //external api
+    @Bean
+    public CustomerCheckApi  customerCheckApi(CustomerRepository customerRepository) {
+        return new CustomerCheckApiImpl(customerRepository);
+    }
 }
