a7eff46bef618a5a39904f19f49c199b833b2cbf
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 19:23:57 +0530

    feat: setup create delivery endpoint

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/controllers/DeliveryController.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/controllers/DeliveryController.java
@@ -1,7 +1,16 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.controllers;
 
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.DeliveryUseCase;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.webMappers.DeliveryWebMapper;
+import jakarta.validation.Valid;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.CreateDeliveryUseCase;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CreateDeliveryCommand;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CreateDeliveryResult;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.CreateDeliveryRequestDTO;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.CreateDeliveryResponseDTO;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.webMappers.CreateDeliveryWebMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryController {
 
     //inject required dependencies
-    private final DeliveryUseCase  deliveryUseCase;
-    private final DeliveryWebMapper deliveryWebMapper;
+    private final CreateDeliveryUseCase createDeliveryUseCase;
+    private final CreateDeliveryWebMapper createDeliveryWebMapper;
 
     public DeliveryController(
-            DeliveryUseCase deliveryUseCase,
-            DeliveryWebMapper deliveryWebMapper
+            CreateDeliveryUseCase createDeliveryUseCase,
+            CreateDeliveryWebMapper createDeliveryWebMapper
     ) {
-        this.deliveryUseCase = deliveryUseCase;
-        this.deliveryWebMapper = deliveryWebMapper;
+        this.createDeliveryUseCase = createDeliveryUseCase;
+        this.createDeliveryWebMapper = createDeliveryWebMapper;
+    }
+
+    //create delivery
+    @PostMapping
+    public ResponseEntity<CreateDeliveryResponseDTO> createDelivery(
+            @Valid @RequestBody CreateDeliveryRequestDTO createDeliveryRequestDTO
+            ){
+
+        CreateDeliveryCommand toCommand = createDeliveryWebMapper.toDeliveryCommand(createDeliveryRequestDTO);
+        CreateDeliveryResult toUseCase = createDeliveryUseCase.createDelivery(toCommand);
+        CreateDeliveryResponseDTO responseDTO = createDeliveryWebMapper.toDeliveryResponseDTO(toUseCase);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 }
