c6dc2cc6a736fb6e83214309b597b1f0a98688ca
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 18:58:56 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/controllers/DeliveryController.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/controllers/DeliveryController.java
@@ -1,4 +1,23 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.controllers;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.DeliveryUseCase;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.webMappers.DeliveryWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/deliveries")
 public class DeliveryController {
+
+    //inject required dependencies
+    private final DeliveryUseCase  deliveryUseCase;
+    private final DeliveryWebMapper deliveryWebMapper;
+
+    public DeliveryController(
+            DeliveryUseCase deliveryUseCase,
+            DeliveryWebMapper deliveryWebMapper
+    ) {
+        this.deliveryUseCase = deliveryUseCase;
+        this.deliveryWebMapper = deliveryWebMapper;
+    }
 }
