cde148e103121af3e1c4c576fa0481a36f61be9f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 20:50:20 +0530

    feat: implement customer details return class for other domain usage

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/usecase/api/CustomerCheckApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/usecase/api/CustomerCheckApiImpl.java
@@ -1,7 +1,12 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.usecase.api;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.api.CustomerCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.models.Customer;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.repositories.CustomerRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.CustomerCheckApiDTO;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class CustomerCheckApiImpl implements CustomerCheckApi {
 
@@ -10,5 +15,25 @@ public class CustomerCheckApiImpl implements CustomerCheckApi {
 
     public CustomerCheckApiImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    //get customer by id
+    @Override
+    public CustomerCheckApiDTO getCustomerCheck(UUID customerId) {
+
+        if(customerId == null){
+            throw new ResourceNotFoundException("Customer ID cannot be empty");
+        }
+
+        //check customer existence
+        Customer getCustomer = customerRepository.getCustomerById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found with id " + customerId));
+
+        return new CustomerCheckApiDTO(
+                getCustomer.getCustomerId(),
+                getCustomer.getCustomerName(),
+                getCustomer.getCustomerEmail(),
+                getCustomer.getCustomerStatus()
+        );
     }
 }
