c4299f58651917bb737cc1549a95d2a1f8ec5d59
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 19:04:36 +0530

    feat: initiate delivery reponse mapper

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/webMappers/DeliveryWebMapper.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/webMappers/DeliveryWebMapper.java
@@ -1,7 +1,9 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.webMappers;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.DeliveryCommand;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.DeliveryResult;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.DeliveryRequestDTO;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.DeliveryResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +11,7 @@ public interface DeliveryWebMapper {
 
     //dto to command
     DeliveryCommand toDeliveryCommand(DeliveryRequestDTO deliveryRequestDTO);
+
+    //domain model to response
+    DeliveryResponseDTO toDeliveryResponseDTO(DeliveryResult deliveryResult);
 }
