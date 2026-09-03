1245429e8a33f14df5ee1d12bc561ee08099abb8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 19:14:56 +0530

    feat: initiate crete delivery usecase impl as service class through config class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CreateDeliveryUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CreateDeliveryUseCaseImpl.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
-public class CreateDeliveryUseCaseImpl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
+
+public class CreateDeliveryUseCaseImpl implements CreateDeliveryUseCase {
+
+    //inject required dependencies
+    private final DeliveryRepository deliveryRepository;
+
+    public CreateDeliveryUseCaseImpl(DeliveryRepository deliveryRepository) {
+        this.deliveryRepository=deliveryRepository;
+    }
+
 }
