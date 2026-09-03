edc3a77f514e5aeb1d189f4970a381521f78cba7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 19:33:55 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/CustomerRepositoryImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/CustomerRepositoryImpl.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence;
 
-public class CustomerRepositoryImpl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.repositories.CustomerRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.jpa.JpaCustomerRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.persistenceMappers.CustomerPersistenceMapper;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    //inject required dependencies
+    private final JpaCustomerRepository jpaCustomerRepository;
+    private final CustomerPersistenceMapper customerPersistenceMapper;
+
+    public CustomerRepositoryImpl(
+            JpaCustomerRepository jpaCustomerRepository,
+            CustomerPersistenceMapper customerPersistenceMapper
+    ) {
+        this.jpaCustomerRepository = jpaCustomerRepository;
+        this.customerPersistenceMapper = customerPersistenceMapper;
+    }
 }
