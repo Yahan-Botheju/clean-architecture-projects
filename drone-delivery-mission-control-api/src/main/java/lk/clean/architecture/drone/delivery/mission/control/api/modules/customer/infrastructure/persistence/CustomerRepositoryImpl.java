5afb4bf0c8ff0ca150835a87ef51844ca01060f2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 20:44:35 +0530

    feat: get customer from db

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/CustomerRepositoryImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/persistence/CustomerRepositoryImpl.java
@@ -1,8 +1,12 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.models.Customer;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.repositories.CustomerRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.jpa.JpaCustomerRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.persistence.persistenceMappers.CustomerPersistenceMapper;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
 
@@ -16,5 +20,12 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     ) {
         this.jpaCustomerRepository = jpaCustomerRepository;
         this.customerPersistenceMapper = customerPersistenceMapper;
+    }
+
+    //get customer by id
+    @Override
+    public Optional<Customer> getCustomer(UUID customerId) {
+        return jpaCustomerRepository.findById(customerId)
+                .map(customerPersistenceMapper::toDomainModel);
     }
 }
