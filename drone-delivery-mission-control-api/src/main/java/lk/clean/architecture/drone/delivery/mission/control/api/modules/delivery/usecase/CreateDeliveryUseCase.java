c5350de22638b4bcb6fe387addd4ded11e00ddeb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-03 19:11:38 +0530

    feat: initiate create delivery method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CreateDeliveryUseCase.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CreateDeliveryUseCase.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CreateDeliveryCommand;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CreateDeliveryResult;
+
 public interface CreateDeliveryUseCase {
+
+    //create a delivery
+    CreateDeliveryResult createDelivery(CreateDeliveryCommand createDeliveryCommand);
 }
