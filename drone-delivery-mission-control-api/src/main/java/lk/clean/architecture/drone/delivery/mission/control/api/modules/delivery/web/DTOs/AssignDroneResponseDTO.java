bf697fd024766634e4a35e9af79426ff711a0a75
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-04 19:12:09 +0530

    feat: create assign drone response dto

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/AssignDroneResponseDTO.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/AssignDroneResponseDTO.java
@@ -1,4 +1,27 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.enums.DeliveryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssignDroneResponseDTO {
+    private UUID deliveryId;
+    private UUID customerId;
+    private UUID assignedDroneId;
+    private double packageWeightKg;
+    private String pickupLocation;
+    private String deliveryLocation;
+    private DeliveryStatus deliveryStatus;
+    private LocalDateTime requestedAt;
+    private LocalDateTime scheduledAt;
+    private LocalDateTime completedAt;
+    private LocalDateTime failedAt;
+    private LocalDateTime cancelledAt;
 }
