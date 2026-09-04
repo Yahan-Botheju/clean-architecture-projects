1e959928691c6f50ed6a7bfbfc65f24505e1ae74
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-04 12:29:46 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DronePackageWeightCheckApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DronePackageWeightCheckApiImpl.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
 
-public class DronePackageWeightCheckApiImpl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DronePackageWeightCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+
+public class DronePackageWeightCheckApiImpl implements DronePackageWeightCheckApi {
+
+    //inject required dependencies
+    private final DroneRepository droneRepository;
+
+    public DronePackageWeightCheckApiImpl(DroneRepository droneRepository) {
+        this.droneRepository = droneRepository;
+    }
 }
