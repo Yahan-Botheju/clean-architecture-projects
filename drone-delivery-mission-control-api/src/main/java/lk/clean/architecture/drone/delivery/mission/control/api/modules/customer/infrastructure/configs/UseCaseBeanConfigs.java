dd847608d95484c50428e7c43eeeae0552e9fab7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-04 11:52:06 +0530

    fix: wrongly assign class clear and add actual one

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/customer/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,8 +1,8 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.configs;
 
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.api.CustomerCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.api.CustomerStatusCheckApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.domain.repositories.CustomerRepository;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.usecase.api.CustomerCheckApiImpl;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.usecase.api.CustomerStatusCheckApiImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +11,7 @@ public class UseCaseBeanConfigs {
 
     //external api
     @Bean
-    public CustomerCheckApi  customerCheckApi(CustomerRepository customerRepository) {
-        return new CustomerCheckApiImpl(customerRepository);
+    public CustomerStatusCheckApi customerStatusCheckApi(CustomerRepository customerRepository) {
+        return new CustomerStatusCheckApiImpl(customerRepository);
     }
 }
