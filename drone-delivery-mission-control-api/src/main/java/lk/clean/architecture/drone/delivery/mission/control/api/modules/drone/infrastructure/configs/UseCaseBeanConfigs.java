c2e3c66b054f45c51b114bebe308191c46a4a785
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-04 12:33:36 +0530

    feat: initiate drone package weight check api impl as service class through config class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,10 +1,12 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.infrastructure.configs;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DronePackageWeightCheckApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskCompleteApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneTaskFailApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api.DroneAssignApiImpl;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api.DronePackageWeightCheckApiImpl;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api.DroneTaskCompleteApiImpl;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api.DroneTaskFailApiImpl;
 import org.springframework.context.annotation.Bean;
@@ -35,5 +37,13 @@ public class UseCaseBeanConfigs {
             DroneRepository droneRepository
     ){
         return new DroneTaskFailApiImpl(droneRepository);
+    }
+
+    //external usage api
+    @Bean
+    public DronePackageWeightCheckApi dronePackageWeightCheckApi(
+            DroneRepository droneRepository
+    ) {
+        return new DronePackageWeightCheckApiImpl(droneRepository);
     }
 }
