46652c91e0ad1fc847b83602663b7ff55ad34ba7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-05 19:10:40 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneExistenceCheck.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneExistenceCheck.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneExistenceCheckApiDTO;
+
+import java.util.UUID;
+
 public interface DroneExistenceCheck {
+
+    DroneExistenceCheckApiDTO checkDroneExistence(UUID droneId);
 }
