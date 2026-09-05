f3ded51b44851953cc9dec3d0b8d9e781539f136
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-05 19:11:28 +0530

    feat: create drone existence check api impl functinality

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneExistenceCheckApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneExistenceCheckApiImpl.java
@@ -1,7 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheck;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneExistenceCheckApiDTO;
+
+import java.util.UUID;
 
 public class DroneExistenceCheckApiImpl extends AbstractDroneFindSupport implements DroneExistenceCheck {
 
@@ -10,6 +14,22 @@ public class DroneExistenceCheckApiImpl extends AbstractDroneFindSupport impleme
             DroneRepository droneRepository
     ) {
         super(droneRepository);
+    }
+
+    //check drone existence
+    @Override
+    public DroneExistenceCheckApiDTO checkDroneExistence(UUID droneId) {
+
+        //check drone existence
+        Drone checkDroneExistence = getDroneById(droneId);
+
+        return new DroneExistenceCheckApiDTO(
+                checkDroneExistence.getDroneId(),
+                checkDroneExistence.getSerialNumber(),
+                checkDroneExistence.getDroneStatus(),
+                checkDroneExistence.getMaxPayloadingKg(),
+                checkDroneExistence.getBatteryPercentage()
+        );
     }
 
 }
