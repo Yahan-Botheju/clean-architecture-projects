b929c5d0d5ff7b61ccda49713aa04887c56b323c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-05 20:26:33 +0530

    feat: inject required dependency

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/AssignDroneUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/AssignDroneUseCaseImpl.java
@@ -5,6 +5,9 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.AssignDroneResult;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DronePackageWeightCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums.DroneStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneExistenceCheckApiDTO;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
@@ -14,13 +17,16 @@ public class AssignDroneUseCaseImpl implements AssignDroneUseCase {
     //inject required dependencies
     private final DeliveryRepository deliveryRepository;
     private final DroneExistenceCheckApi droneExistenceCheckApi;
+    private final DronePackageWeightCheckApi dronePackageWeightCheckApi;
 
     public AssignDroneUseCaseImpl(
             DeliveryRepository deliveryRepository,
-            DroneExistenceCheckApi droneExistenceCheckApi
+            DroneExistenceCheckApi droneExistenceCheckApi,
+            DronePackageWeightCheckApi dronePackageWeightCheckApi
     ) {
         this.deliveryRepository = deliveryRepository;
         this.droneExistenceCheckApi = droneExistenceCheckApi;
+        this.dronePackageWeightCheckApi = dronePackageWeightCheckApi;
     }
 
     //assign a drone
@@ -32,10 +38,18 @@ public class AssignDroneUseCaseImpl implements AssignDroneUseCase {
                 .orElseThrow(() -> new ResourceNotFoundException("Delivery with id: " + droneId + " not found."));
 
         //check delivery state is REQUESTED
-        if(deliveryExistence.getDeliveryStatus() !=  DeliveryStatus.REQUESTED
-
-        ) {
+        boolean isRequested = deliveryExistence.getDeliveryStatus() == DeliveryStatus.REQUESTED;
+        if(!isRequested) {
             throw new IllegalStateException("Delivery status is not proper state to continue the process");
+        }
+
+        //check drone existence
+        DroneExistenceCheckApiDTO checkDrone = droneExistenceCheckApi.checkDroneExistence(droneId);
+
+        //check drone status is AVAILABLE
+        boolean isDroneAvailable = checkDrone.droneStatus() == DroneStatus.AVAILABLE;
+        if(!isDroneAvailable) {
+            throw new IllegalStateException("Drone is not available");
         }
 
     }
