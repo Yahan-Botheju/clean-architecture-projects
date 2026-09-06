da580bcc8c6ebbe98866198f80b3af6208cbc3ce
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-06 13:54:04 +0530

    feat: create required functioncality to check weather condition through the domain logic used

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/CheckFlightSafetyRule1Impl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/CheckFlightSafetyRule1Impl.java
@@ -1,4 +1,27 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.usecase.api;
 
-public class CheckFlightSafetyRule1Impl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.models.OperationalAssessment;
+
+public class CheckFlightSafetyRule1Impl implements CheckFlightSafetyRule1 {
+
+    //check flight safety rule 1
+    @Override
+    public boolean checkFlightSafetyRule1(
+            WeatherCondition weatherCondition,
+            AirSpaceStatus airSpaceStatus
+    ){
+        //create model
+        OperationalAssessment newOperation =  new OperationalAssessment(
+                weatherCondition,
+                airSpaceStatus
+        );
+
+        //check domain logic
+        newOperation.checkFlightSafetyRule1();
+
+        return newOperation.isAllowed();
+    }
 }
