4a5baaef2c099222918a63a9fee99f9c2b07457c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-06 13:53:04 +0530

    feat: flight safety rule 1 impl class registered as service class through config class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,16 @@
-package lk.clean.architecture.drone.delivery.mission.control.api.modules.customer.infrastructure.configs;
+package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.infrastructure.configs;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.usecase.api.CheckFlightSafetyRule1Impl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    //flight safety rule 1
+    @Bean
+    public CheckFlightSafetyRule1 checkFlightSafetyRule1(){
+        return new CheckFlightSafetyRule1Impl();
+    }
 }
