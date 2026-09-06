095a0dfeb346482ffe15aad37b4542089d6075f4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-06 19:20:28 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/BatteryLevelCheckRainyWeather.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/BatteryLevelCheckRainyWeather.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
+
 public interface BatteryLevelCheckRainyWeather {
+
+    //check current battery level in rainy weather
+    public boolean batteryLevelCheckRainyWeather(WeatherCondition weatherCondition, AirSpaceStatus airSpaceStatus, double currentBatteryLevel);
 }
