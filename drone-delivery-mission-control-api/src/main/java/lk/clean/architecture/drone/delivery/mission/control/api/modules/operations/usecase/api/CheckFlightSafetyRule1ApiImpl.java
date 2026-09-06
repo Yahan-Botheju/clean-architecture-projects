1bcb3fba40a5339040b3c2e1c90bda3b2a749a17
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-06 20:09:06 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/CheckFlightSafetyRule1ApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/CheckFlightSafetyRule1ApiImpl.java
@@ -1,27 +1,34 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.usecase.api;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1Api;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.models.OperationalAssessment;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.repositories.OperationAssementRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.repositories.AirStatusOperationAssessmentRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.repositories.WeatherConditionOperationAssessmentRepository;
 
 public class CheckFlightSafetyRule1ApiImpl implements CheckFlightSafetyRule1Api {
 
     //inject required dependencies
-    private final OperationAssementRepository operationAssementRepository;
+    private final WeatherConditionOperationAssessmentRepository weatherConditionOperationAssessmentRepository;
+    private final AirStatusOperationAssessmentRepository airStatusOperationAssessmentRepository;
 
-    public CheckFlightSafetyRule1ApiImpl(OperationAssementRepository operationAssementRepository) {
-        this.operationAssementRepository = operationAssementRepository;
+    public CheckFlightSafetyRule1ApiImpl(
+            WeatherConditionOperationAssessmentRepository weatherConditionOperationAssessmentRepository,
+            AirStatusOperationAssessmentRepository airStatusOperationAssessmentRepository)
+    {
+        this.weatherConditionOperationAssessmentRepository = weatherConditionOperationAssessmentRepository;
+        this.airStatusOperationAssessmentRepository = airStatusOperationAssessmentRepository;
     }
 
 
     //check flight safety rule 1
     @Override
-    public boolean checkFlightSafetyRule1(
-            WeatherCondition weatherCondition,
-            AirSpaceStatus airSpaceStatus
-    ){
+    public boolean checkFlightSafetyRule1(String pickUpLocation, String deliveryLocation){
+
+        //pick operation assessment
+
+
+
+
         //create model using factor method
         OperationalAssessment newOperation =  OperationalAssessment.createOperationalAssessment(
                 weatherCondition,
