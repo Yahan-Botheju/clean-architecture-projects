1e221b3e620472a7c02afbd9e342c85c7c0bbbcf
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-06 13:52:24 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/CheckFlightSafetyRule1.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/CheckFlightSafetyRule1.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api;
 
-public interface CheckFlightSaftyRule1 {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
+
+public interface CheckFlightSafetyRule1 {
+
+    //check flight safety rule 1
+    public boolean checkFlightSafetyRule1(WeatherCondition weatherCondition, AirSpaceStatus airSpaceStatus);
 }
