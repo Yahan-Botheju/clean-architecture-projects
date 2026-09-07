aced3aca92e81f5f2a9d7f63e19e76c1c246a0a2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 20:58:33 +0530

    feat: create assign drone usecase impl required functionality

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/AssignDroneUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/AssignDroneUseCaseImpl.java
@@ -8,12 +8,13 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.ap
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DronePackageWeightCheckApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums.DroneStatus;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.BatteryLevelCheckRainyWeather;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule2;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.BatteryLevelCheckRainyWeatherApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1Api;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule2Api;
 import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneExistenceCheckApiDTO;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class AssignDroneUseCaseImpl implements AssignDroneUseCase {
@@ -23,26 +24,26 @@ public class AssignDroneUseCaseImpl implements AssignDroneUseCase {
     private final DroneExistenceCheckApi droneExistenceCheckApi;
     private final DronePackageWeightCheckApi dronePackageWeightCheckApi;
     private final DroneBatteryCheckApi droneBatteryCheckApi;
-    private final BatteryLevelCheckRainyWeather batteryLevelCheckRainyWeather;
-    private final CheckFlightSafetyRule1 checkFlightSafetyRule1;
-    private final CheckFlightSafetyRule2 checkFlightSafetyRule2;
+    private final BatteryLevelCheckRainyWeatherApi batteryLevelCheckRainyWeatherApi;
+    private final CheckFlightSafetyRule1Api checkFlightSafetyRule1Api;
+    private final CheckFlightSafetyRule2Api checkFlightSafetyRule2Api;
 
     public AssignDroneUseCaseImpl(
             DeliveryRepository deliveryRepository,
             DroneExistenceCheckApi droneExistenceCheckApi,
             DronePackageWeightCheckApi dronePackageWeightCheckApi,
             DroneBatteryCheckApi droneBatteryCheckApi,
-            BatteryLevelCheckRainyWeather batteryLevelCheckRainyWeather,
-            CheckFlightSafetyRule1 checkFlightSafetyRule1,
-            CheckFlightSafetyRule2 checkFlightSafetyRule2
+            BatteryLevelCheckRainyWeatherApi batteryLevelCheckRainyWeatherApi,
+            CheckFlightSafetyRule1Api checkFlightSafetyRule1Api,
+            CheckFlightSafetyRule2Api checkFlightSafetyRule2Api
     ) {
         this.deliveryRepository = deliveryRepository;
         this.droneExistenceCheckApi = droneExistenceCheckApi;
         this.dronePackageWeightCheckApi = dronePackageWeightCheckApi;
         this.droneBatteryCheckApi = droneBatteryCheckApi;
-        this.batteryLevelCheckRainyWeather = batteryLevelCheckRainyWeather;
-        this.checkFlightSafetyRule1 = checkFlightSafetyRule1;
-        this.checkFlightSafetyRule2 = checkFlightSafetyRule2;
+        this.batteryLevelCheckRainyWeatherApi = batteryLevelCheckRainyWeatherApi;
+        this.checkFlightSafetyRule1Api = checkFlightSafetyRule1Api;
+        this.checkFlightSafetyRule2Api = checkFlightSafetyRule2Api;
     }
 
     //assign a drone
@@ -50,30 +51,64 @@ public class AssignDroneUseCaseImpl implements AssignDroneUseCase {
     public AssignDroneResult assignDrone(UUID droneId){
 
         //check delivery existence
-        Delivery deliveryExistence = deliveryRepository.getDeliveryById(droneId)
+        Delivery delivery = deliveryRepository.getDeliveryById(droneId)
                 .orElseThrow(() -> new ResourceNotFoundException("Delivery with id: " + droneId + " not found."));
 
         //check delivery state is REQUESTED
-        boolean isRequested = deliveryExistence.getDeliveryStatus() == DeliveryStatus.REQUESTED;
+        boolean isRequested = delivery.getDeliveryStatus() == DeliveryStatus.REQUESTED;
         if(!isRequested) {
             throw new IllegalStateException("Delivery status is not proper state to continue the process");
         }
 
         //check drone existence
-        DroneExistenceCheckApiDTO checkDrone = droneExistenceCheckApi.checkDroneExistence(droneId);
+        DroneExistenceCheckApiDTO drone = droneExistenceCheckApi.checkDroneExistence(droneId);
 
         //check drone status is AVAILABLE
-        boolean isDroneAvailable = checkDrone.droneStatus() == DroneStatus.AVAILABLE;
+        boolean isDroneAvailable = drone.droneStatus() == DroneStatus.AVAILABLE;
         if(!isDroneAvailable) {
             throw new IllegalStateException("Drone is not available");
         }
 
         //check drone payload
-        dronePackageWeightCheckApi.checkDronePackageWeight(checkDrone.droneId(), deliveryExistence.getPackageWeightKg());
+        dronePackageWeightCheckApi.checkDronePackageWeight(drone.droneId(), delivery.getPackageWeightKg());
 
         //check drone battery status
-        droneBatteryCheckApi.checkDroneBatteryStatus(checkDrone.droneId(), deliveryExistence.getPackageWeightKg());
+        droneBatteryCheckApi.checkDroneBatteryStatus(drone.droneId(), delivery.getPackageWeightKg());
+
+        /*
+        * _OPERATION_DOMAIN_VALIDATIONS
+        * */
+
+        checkFlightSafetyRule1Api.checkFlightSafetyRule1(delivery.getPickupLocation(),delivery.getDeliveryLocation());
+        checkFlightSafetyRule2Api.checkFlightSafetyRule2(delivery.getPickupLocation(),delivery.getDeliveryLocation());
+        batteryLevelCheckRainyWeatherApi.batteryLevelCheckRainyWeather(
+                delivery.getPickupLocation(),
+                delivery.getDeliveryLocation(),
+                drone.batteryPercentage()
+        );
+
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        //set delivery to SCHEDULE
+        delivery.scheduleDrone(currentTime);
+
+        //assign a drone
+        delivery.assignDrone(drone.droneId(), currentTime);
 
 
+        return new AssignDroneResult(
+                delivery.getDeliveryId(),
+                delivery.getCustomerId(),
+                delivery.getAssignedDroneId(),
+                delivery.getPackageWeightKg(),
+                delivery.getPickupLocation(),
+                delivery.getDeliveryLocation(),
+                delivery.getDeliveryStatus(),
+                delivery.getRequestedAt(),
+                delivery.getScheduledAt(),
+                delivery.getCompletedAt(),
+                delivery.getFailedAt(),
+                delivery.getCancelledAt()
+        );
     }
 }
