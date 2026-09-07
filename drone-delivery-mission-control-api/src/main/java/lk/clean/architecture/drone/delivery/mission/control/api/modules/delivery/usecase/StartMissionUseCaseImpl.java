4a8b7f03e0ebcc526687b040da90082586a410d9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 21:57:47 +0530

    feat: inject required dependencis

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
@@ -1,13 +1,40 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.enums.DeliveryStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.models.Delivery;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class StartMissionUseCaseImpl implements StartMissionUseCase {
 
     //inject required dependencies
     private final DeliveryRepository deliveryRepository;
+    private final DroneAssignApi  droneAssignApi;
 
-    public StartMissionUseCaseImpl(DeliveryRepository deliveryRepository) {
+
+    public StartMissionUseCaseImpl(DeliveryRepository deliveryRepository,  DroneAssignApi droneAssignApi) {
         this.deliveryRepository=deliveryRepository;
+        this.droneAssignApi=droneAssignApi;
+    }
+
+    @Override
+    public void startMission(UUID deliveryId) {
+
+        Delivery getDelivery = deliveryRepository.getDeliveryById(deliveryId)
+                .orElseThrow(()->new ResourceNotFoundException("Delivery Not Found"));
+
+        //check delivery status == SCHEDULE
+        boolean checkDeliveryStatus = getDelivery.getDeliveryStatus() == DeliveryStatus.SCHEDULED;
+
+        if(!checkDeliveryStatus)
+            throw new ResourceNotFoundException("cannot process delivery under this status");
+
+        //check assigned drone is available
+
+
+
     }
 }
