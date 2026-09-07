97b4a782f08f949d145a8b915387d9e649bbba3c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 21:14:06 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCase.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCase.java
@@ -1,4 +1,9 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import java.util.UUID;
+
 public interface StartMissionUseCase {
+
+    //start mission
+    void startMission(UUID deliveryId);
 }
