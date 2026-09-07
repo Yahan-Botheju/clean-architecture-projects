23ce7875fcf70babaf2a688a279696f865431817
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 19:52:08 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/BatteryLevelCheckRainyWeatherApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/usecase/api/BatteryLevelCheckRainyWeatherApiImpl.java
@@ -4,16 +4,23 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.operatio
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.models.OperationalAssessment;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.repositories.AirStatusOperationAssessmentRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.repositories.WeatherConditionOperationAssessmentRepository;
 
 public class BatteryLevelCheckRainyWeatherApiImpl implements BatteryLevelCheckRainyWeatherApi {
 
+    //inject required dependencies
+    private final WeatherConditionOperationAssessmentRepository weatherConditionOperationAssessmentRepository;
+    private final AirStatusOperationAssessmentRepository airStatusOperationAssessmentRepository;
+
+    public BatteryLevelCheckRainyWeatherApiImpl(WeatherConditionOperationAssessmentRepository weatherConditionOperationAssessmentRepository, AirStatusOperationAssessmentRepository airStatusOperationAssessmentRepository) {
+        this.weatherConditionOperationAssessmentRepository = weatherConditionOperationAssessmentRepository;
+        this.airStatusOperationAssessmentRepository = airStatusOperationAssessmentRepository;
+    }
+
 
     //check current battery level in rainy weather
-    public boolean batteryLevelCheckRainyWeather(
-            WeatherCondition weatherCondition,
-            AirSpaceStatus airSpaceStatus,
-            double currentBatteryLevel
-    ){
+    public boolean batteryLevelCheckRainyWeather(String pickUpLocation, String deliveryLocation, double currentBatteryLevel){
 
         //create domain model
         OperationalAssessment newOperation = OperationalAssessment.createOperationalAssessment(
