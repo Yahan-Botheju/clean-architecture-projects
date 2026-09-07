f8f5ecf3d2a6ede40e4cbe67d6f1855a9cac019e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 19:36:20 +0530

    feat: initiate dependencies for inject in usecase

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/infrastructure/configs/UseCaseBeanConfigs.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/infrastructure/configs/UseCaseBeanConfigs.java
@@ -25,8 +25,11 @@ public class UseCaseBeanConfigs {
 
     //flight safety rule 2
     @Bean
-    public CheckFlightSafetyRule2Api checkFlightSafetyRule2(){
-        return new CheckFlightSafetyRule2ApiImpl();
+    public CheckFlightSafetyRule2Api checkFlightSafetyRule2(
+            WeatherConditionOperationAssessmentRepository weatherConditionOperationAssessmentRepository,
+            AirStatusOperationAssessmentRepository airStatusOperationAssessmentRepository
+    ){
+        return new CheckFlightSafetyRule2ApiImpl(weatherConditionOperationAssessmentRepository, airStatusOperationAssessmentRepository);
     }
 
     //flight check in rainy weather
