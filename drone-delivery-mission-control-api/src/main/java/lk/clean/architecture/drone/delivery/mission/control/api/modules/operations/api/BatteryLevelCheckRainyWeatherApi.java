eef58a6b10601d8e6024416a0d618249b16ff76c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 19:59:40 +0530

    feat: change parameters

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/BatteryLevelCheckRainyWeatherApi.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/BatteryLevelCheckRainyWeatherApi.java
@@ -6,5 +6,5 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.operatio
 public interface BatteryLevelCheckRainyWeatherApi {
 
     //check current battery level in rainy weather
-    public boolean batteryLevelCheckRainyWeather(WeatherCondition weatherCondition, AirSpaceStatus airSpaceStatus, double currentBatteryLevel);
+    public boolean batteryLevelCheckRainyWeather(String pickUpLocation, String deliveryLocation, double currentBatteryLevel);
 }
