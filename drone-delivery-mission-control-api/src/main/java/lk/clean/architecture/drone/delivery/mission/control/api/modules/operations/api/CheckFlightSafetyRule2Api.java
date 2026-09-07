3deecb4bc7fb914c38a4d70c74d4971c10047a36
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-07 19:47:33 +0530

    feat: change parameters

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/CheckFlightSafetyRule2Api.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/operations/api/CheckFlightSafetyRule2Api.java
@@ -1,10 +1,7 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api;
 
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.AirSpaceStatus;
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.domain.enums.WeatherCondition;
-
 public interface CheckFlightSafetyRule2Api {
 
     //check flight safety rule 2
-    boolean checkFlightSafetyRule2(WeatherCondition weatherCondition, AirSpaceStatus airSpaceStatus);
+    boolean checkFlightSafetyRule2(String pickUpLocation, String deliveryLocation);
 }
