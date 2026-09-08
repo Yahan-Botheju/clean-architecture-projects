1987c80dcbf13165888a42a4c542f537afb3c0ce
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-08 22:37:40 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CompleteMissionUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CompleteMissionUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
-public class CompleteMissionUseCaseImpl {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
+
+public class CompleteMissionUseCaseImpl implements CompleteMissionUseCase {
+
+    //inject required dependencies
+    private final DeliveryRepository deliveryRepository;
+
+    public CompleteMissionUseCaseImpl(DeliveryRepository deliveryRepository) {
+        this.deliveryRepository = deliveryRepository;
+    }
 }
