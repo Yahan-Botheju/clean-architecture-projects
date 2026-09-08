2d4e07e3a521f628fbfa163344eac9049c64872c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-08 20:43:23 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
@@ -5,6 +5,10 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums.DroneStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule1Api;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.operations.api.CheckFlightSafetyRule2Api;
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneExistenceCheckApiDTO;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
@@ -15,16 +19,22 @@ public class StartMissionUseCaseImpl implements StartMissionUseCase {
     private final DeliveryRepository deliveryRepository;
     private final DroneAssignApi  droneAssignApi;
     private final DroneExistenceCheckApi  droneExistenceCheckApi;
+    private final CheckFlightSafetyRule1Api  checkFlightSafetyRule1Api;
+    private final CheckFlightSafetyRule2Api checkFlightSafetyRule2Api;
 
 
     public StartMissionUseCaseImpl(
             DeliveryRepository deliveryRepository,
             DroneAssignApi droneAssignApi,
-            DroneExistenceCheckApi droneExistenceCheckApi
+            DroneExistenceCheckApi droneExistenceCheckApi,
+            CheckFlightSafetyRule1Api checkFlightSafetyRule1Api,
+            CheckFlightSafetyRule2Api checkFlightSafetyRule2Api
     ) {
         this.deliveryRepository=deliveryRepository;
         this.droneAssignApi=droneAssignApi;
         this.droneExistenceCheckApi=droneExistenceCheckApi;
+        this.checkFlightSafetyRule1Api=checkFlightSafetyRule1Api;
+        this.checkFlightSafetyRule2Api=checkFlightSafetyRule2Api;
     }
 
     @Override
@@ -40,8 +50,19 @@ public class StartMissionUseCaseImpl implements StartMissionUseCase {
             throw new ResourceNotFoundException("cannot process delivery under this status");
 
         //check assigned drone is available
+        boolean isDroneAssigned = droneExistenceCheckApi.checkDroneExistenceById(getDelivery.getAssignedDroneId());
 
+        if(!isDroneAssigned){
+            throw new ResourceNotFoundException("cannot process drone under this status");
+        }
 
+        DroneExistenceCheckApiDTO getDroneStaus = droneExistenceCheckApi.checkDroneExistence(getDelivery.getAssignedDroneId());
 
+        //check drone status is AVAILABLE
+        boolean isDroneAvailable = getDroneStaus.droneStatus() == DroneStatus.AVAILABLE;
+
+        if(!isDroneAvailable){
+            throw new IllegalStateException("cannot process drone under this status");
+        }
     }
 }
