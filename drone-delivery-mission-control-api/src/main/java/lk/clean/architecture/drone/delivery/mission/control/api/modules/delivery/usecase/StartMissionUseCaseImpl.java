89e15ca0634286d49b9d4de2197eb34ba5fceb55
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-08 19:08:14 +0530

    feat: injecte required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/StartMissionUseCaseImpl.java
@@ -4,6 +4,7 @@ import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.models.Delivery;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.UUID;
@@ -13,11 +14,17 @@ public class StartMissionUseCaseImpl implements StartMissionUseCase {
     //inject required dependencies
     private final DeliveryRepository deliveryRepository;
     private final DroneAssignApi  droneAssignApi;
+    private final DroneExistenceCheckApi  droneExistenceCheckApi;
 
 
-    public StartMissionUseCaseImpl(DeliveryRepository deliveryRepository,  DroneAssignApi droneAssignApi) {
+    public StartMissionUseCaseImpl(
+            DeliveryRepository deliveryRepository,
+            DroneAssignApi droneAssignApi,
+            DroneExistenceCheckApi droneExistenceCheckApi
+    ) {
         this.deliveryRepository=deliveryRepository;
         this.droneAssignApi=droneAssignApi;
+        this.droneExistenceCheckApi=droneExistenceCheckApi;
     }
 
     @Override
