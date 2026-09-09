b65c3936709f6907945eb274eb019fd6d79b462e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-09 19:32:19 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CompleteMissionUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CompleteMissionUseCaseImpl.java
@@ -1,13 +1,42 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.enums.DeliveryStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.models.Delivery;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
+
+import java.util.UUID;
 
 public class CompleteMissionUseCaseImpl implements CompleteMissionUseCase {
 
     //inject required dependencies
     private final DeliveryRepository deliveryRepository;
+    private final DroneExistenceCheckApi droneExistenceCheckApi;
 
-    public CompleteMissionUseCaseImpl(DeliveryRepository deliveryRepository) {
+    public CompleteMissionUseCaseImpl(
+            DeliveryRepository deliveryRepository,
+            DroneExistenceCheckApi droneExistenceCheckApi
+
+    ) {
         this.deliveryRepository = deliveryRepository;
+        this.droneExistenceCheckApi = droneExistenceCheckApi;
+    }
+
+    //complete mission
+    @Override
+    public void completeMission(UUID deliveryId) {
+
+        Delivery getDelivery = deliveryRepository.getDeliveryById(deliveryId)
+                .orElseThrow(() -> new RuntimeException("delivery not found"));
+
+        //check delivery status IN_PROGRESS
+        boolean isDeliveryInProgress = getDelivery.getDeliveryStatus() == DeliveryStatus.IN_PROGRESS;
+
+        if(!isDeliveryInProgress){
+            throw new IllegalStateException("delivery is not in progress");
+        }
+
+        //check dron status ON_MISSION
+
     }
 }
