19bfd5f8dbeb629b9e36231c90f62eddf4b54277
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-09 20:35:57 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneBatteryConsumptionCheckApi.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/api/DroneBatteryConsumptionCheckApi.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneBatteryCheckApiDTO;
+
+import java.util.UUID;
+
 public interface DroneBatteryConsumptionCheckApi {
+
+    //initiate battery consumption check method
+    DroneBatteryCheckApiDTO droneBatteryConsumptionCheckApi(UUID droneId, double packageWeightKg);
 }
