32957d20cafddd184b3887f3f4844e0e5d4c1806
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-09 20:36:30 +0530

    feat: create functionality of drone battery consumption check api impl

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneBatteryConsumptionCheckApiImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/usecase/api/DroneBatteryConsumptionCheckApiImpl.java
@@ -1,12 +1,36 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.usecase.api;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneBatteryCheckApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneBatteryConsumptionCheckApi;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.models.Drone;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.repositories.DroneRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer.DroneBatteryCheckApiDTO;
+
+import java.util.UUID;
 
 public class DroneBatteryConsumptionCheckApiImpl extends AbstractDroneFindSupport implements DroneBatteryConsumptionCheckApi {
 
     //inject required dependencies
     public DroneBatteryConsumptionCheckApiImpl(DroneRepository droneRepository) {
         super(droneRepository);
+    }
+
+    //initiate battery consumption check method
+    @Override
+    public DroneBatteryCheckApiDTO droneBatteryConsumptionCheckApi(
+            UUID droneId,
+            double packageWeightKg
+    ) {
+
+        Drone getDrone = getDroneById(droneId);
+        //call domain logic
+        getDrone.reduceDroneBattery(packageWeightKg);
+
+        return new DroneBatteryCheckApiDTO(
+                getDrone.getDroneId(),
+                getDrone.getSerialNumber(),
+                getDrone.getDroneStatus(),
+                getDrone.getBatteryPercentage(),
+        );
     }
 }
