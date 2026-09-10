2729874bc613633d1079838f100908cc1e6b9060
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-10 11:58:06 +0530

    feat: initiate failed mission  web mapper components

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/webMappers/DeliveryMissionWebMapper.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/webMappers/DeliveryMissionWebMapper.java
@@ -1,15 +1,19 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.webMappers;
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CompleteMissionResult;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.FailedMissionCommand;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.FailedMissionResult;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.StartMissionResult;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.CompleteMissionResponseDTO;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.FailedMissionRequestDTO;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.FailedMissionResponseDTO;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs.StartMissionResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DeliveryMissionWebMapper {
 
-    /* __CREATE_MISSION__ */
+    /* __START_MISSION__ */
 
     //domain model to response dto
     StartMissionResponseDTO toStartMissionResponse(StartMissionResult startMissionResult);
@@ -19,5 +23,14 @@ public interface DeliveryMissionWebMapper {
 
     //domain model to responseDTO
     CompleteMissionResponseDTO toCompleteMissionResponse(CompleteMissionResult completeMissionResult);
+
+
+    /* __COMPLETE_MISSION__ */
+
+    //requestDTO to usecase object
+    FailedMissionCommand toFailedMissionCommand(FailedMissionRequestDTO failedMissionRequestDTO);
+
+    //domain model to responseDTO
+    FailedMissionResponseDTO toFailedMissionResponseDto(FailedMissionResult failedMissionResult);
 
 }
