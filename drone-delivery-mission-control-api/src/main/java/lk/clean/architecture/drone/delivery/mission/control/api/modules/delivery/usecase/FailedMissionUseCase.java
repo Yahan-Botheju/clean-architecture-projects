7c071410b55f7819602067ba12ed618f5897d89c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-10 11:14:58 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/FailedMissionUseCase.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/FailedMissionUseCase.java
@@ -1,4 +1,12 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.enums.FailureReasonStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.FailedMissionResult;
+
+import java.util.UUID;
+
 public interface FailedMissionUseCase {
+
+    //mission failed
+    FailedMissionResult failedMission(UUID deliveryId, FailureReasonStatus failureReason);
 }
