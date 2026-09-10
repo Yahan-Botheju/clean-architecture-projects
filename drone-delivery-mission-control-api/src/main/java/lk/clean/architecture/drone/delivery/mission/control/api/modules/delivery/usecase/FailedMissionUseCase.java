378dcc1b466de71b31489ff297440a8b3061af27
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-10 12:11:03 +0530

    ref: change the parameters

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/FailedMissionUseCase.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/FailedMissionUseCase.java
@@ -1,12 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
-import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.enums.FailureReasonStatus;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.FailedMissionCommand;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.FailedMissionResult;
 
-import java.util.UUID;
 
 public interface FailedMissionUseCase {
 
     //mission failed
-    FailedMissionResult failedMission(UUID deliveryId, FailureReasonStatus failureReason);
+    FailedMissionResult failedMission(FailedMissionCommand failedMissionCommand);
 }
