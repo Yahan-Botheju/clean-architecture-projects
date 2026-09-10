11241528ed745cc3e226bc1147ff7aa651a2a8f5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-10 10:54:16 +0530

    feat: create failed mission result record class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/records/FailedMissionResult.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/records/FailedMissionResult.java
@@ -1,4 +1,22 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records;
 
-public record FailedMissionResult() {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.enums.DeliveryStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record FailedMissionResult(
+        UUID deliveryId,
+        UUID customerId,
+        UUID assignedDroneId,
+        double packageWeightKg,
+        String pickupLocation,
+        String deliveryLocation,
+        DeliveryStatus deliveryStatus,
+        LocalDateTime requestedAt,
+        LocalDateTime scheduledAt,
+        LocalDateTime completedAt,
+        LocalDateTime failedAt,
+        LocalDateTime cancelledAt
+) {
 }
