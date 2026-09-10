8109a8b5f1d3eae5a489bff2904ab7ab1abc990b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-10 10:53:38 +0530

    feat: create failed mission request and response dto

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/FailedMissionRequestDTO.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/web/DTOs/FailedMissionRequestDTO.java
@@ -1,4 +1,15 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.web.DTOs;
 
+import jakarta.validation.constraints.NotNull;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.enums.FailureReasonStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FailedMissionRequestDTO {
+    @NotNull(message = "Reason cannot be empty")
+    private FailureReasonStatus failureReasonStatus;
 }
