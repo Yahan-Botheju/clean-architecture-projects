5a6d7165dad0e586914987638894502cb9f30596
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-11 10:48:17 +0530

    feat: inject required dependencies

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CancelDeliveryUseCaseImpl.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CancelDeliveryUseCaseImpl.java
@@ -2,6 +2,9 @@ package lk.clean.architecture.drone.delivery.mission.control.api.modules.deliver
 
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.models.Delivery;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.domain.repositories.DeliveryRepository;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.AssignDroneResult;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CancelDeliveryResult;
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneAssignApi;
 import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.api.DroneExistenceCheckApi;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
@@ -12,26 +15,15 @@ public class CancelDeliveryUseCaseImpl implements CancelDeliveryUseCase {
 
     //inject required dependencies
     private final DeliveryRepository deliveryRepository;
-    private final DroneExistenceCheckApi  droneExistenceCheckApi;
+    private final DroneAssignApi droneAssignApi;
 
     public CancelDeliveryUseCaseImpl(
             DeliveryRepository deliveryRepository,
-            DroneExistenceCheckApi droneExistenceCheckApi
+            DroneAssignApi droneAssignApi
     ) {
         this.deliveryRepository = deliveryRepository;
-        this.droneExistenceCheckApi = droneExistenceCheckApi;
+        this.droneAssignApi = droneAssignApi;
     }
 
-    public void cancelDelivery(UUID deliveryId) {
 
-        Delivery delivery = deliveryRepository.getDeliveryById(deliveryId)
-                .orElseThrow(() -> new ResourceNotFoundException("Delivery "+deliveryId+" not found"));
-
-        LocalDateTime currentTime = LocalDateTime.now();
-        //call domain cancel logic
-        delivery.cancelDelivery(currentTime);
-
-        //check drone is assign to mission
-
-    }
 }
