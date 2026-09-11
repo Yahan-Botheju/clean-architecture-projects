fd798281d0c3ab46115f940200720b3ddb183192
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-11 10:46:19 +0530

    feat: create new record class

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/shared_domain/customer/DroneStatusApiDTO.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/shared_domain/customer/DroneStatusApiDTO.java
@@ -1,4 +1,15 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.shared_domain.customer;
 
-public record DroneStatusApiDTO() {
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.domain.enums.DroneStatus;
+
+import java.util.UUID;
+
+public record DroneStatusApiDTO(
+        UUID droneId,
+        String serialNumber,
+        DroneStatus droneStatus,
+        double maxPayloadingKg,
+        double batteryPercentage
+) {
 }
+/* __#6 COMMIT -> create new record class __ DroneStatusApiDTO.java */
