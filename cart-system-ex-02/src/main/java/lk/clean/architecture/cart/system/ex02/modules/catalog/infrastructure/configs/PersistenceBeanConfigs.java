493dc7f4a686b7adf3edf1f5c7fddc87b68fea2c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-11 18:16:27 +0530

    feat: initiate product persistence bean config as component class through the config class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/configs/PersistenceBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/configs/PersistenceBeanConfigs.java
@@ -1,4 +1,21 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.configs;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.ProductRepositoryImpl;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.jpa.JpaProductRepository;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.persistenceMapper.ProductPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PersistenceBeanConfigs {
+
+    //product persistence bean config
+    @Bean
+    public ProductRepository productRepository(
+            JpaProductRepository jpaProductRepository,
+            ProductPersistenceMapper productPersistenceMapper
+    ) {
+        return new ProductRepositoryImpl(jpaProductRepository, productPersistenceMapper);
+    }
 }
