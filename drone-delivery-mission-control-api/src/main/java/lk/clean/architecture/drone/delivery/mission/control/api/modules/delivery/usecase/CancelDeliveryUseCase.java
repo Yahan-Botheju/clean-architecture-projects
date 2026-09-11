14895aae99ea649d8571d845ddcb99f0172b5629
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-11 10:51:22 +0530

    feat: initiate method

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CancelDeliveryUseCase.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/delivery/usecase/CancelDeliveryUseCase.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase;
 
+import lk.clean.architecture.drone.delivery.mission.control.api.modules.delivery.usecase.records.CancelDeliveryResult;
+
+import java.util.UUID;
+
 public interface CancelDeliveryUseCase {
+
+    //cancel delivery
+    CancelDeliveryResult cancelDelivery(UUID deliveryId); /* __#11 COMMIT -> initiate method__   CancelDeliveryUseCase.java */
 }
