b317e17b00dadf57c08de88f1e8c5cebe26516cd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-11 10:44:39 +0530

    add new field

--- drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/domain/enums/DroneStatus.java
+++ drone-delivery-mission-control-api/src/main/java/lk/clean/architecture/drone/delivery/mission/control/api/modules/drone/domain/enums/DroneStatus.java
@@ -2,7 +2,9 @@ package lk.clean.architecture.drone.delivery.mission.control.api.modules.drone.d
 
 public enum DroneStatus {
     AVAILABLE,
+    ASSIGNED,
     ON_MISSION,
     MAINTENANCE,
     OFFLINE
 }
+/* __#4 COMMIT -> add new field */
