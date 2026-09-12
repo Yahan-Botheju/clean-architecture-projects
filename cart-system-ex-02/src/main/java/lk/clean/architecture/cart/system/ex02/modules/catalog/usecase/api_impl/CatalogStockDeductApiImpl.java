6603d6ed62aaf7cdb2d3fc2349691fd193d2a039
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 19:18:10 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogStockDeductApiImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogStockDeductApiImpl.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.api_impl;
 
-public class CatelogStockDeductApiImpl {
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogStockDeductApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
+
+public class CatalogStockDeductApiImpl implements CatalogStockDeductApi {
+
+    //inject required dependencies
+    private final ProductRepository productRepository;
+
+    public CatalogStockDeductApiImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
