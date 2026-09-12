aed09a71328b50cf159dd7584c537f031048d5e7
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 19:07:33 +0530

    feat: initiate create product result record class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/records/CreateProductResult.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/records/CreateProductResult.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.records;
 
-public record CreateProductResult() {
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.enums.ProductStatus;
+
+import java.util.UUID;
+
+public record CreateProductResult(
+         UUID productId,
+         String productName,
+         double unitPrice,
+         int stockQuantity,
+         ProductStatus productStatus
+) {
 }
