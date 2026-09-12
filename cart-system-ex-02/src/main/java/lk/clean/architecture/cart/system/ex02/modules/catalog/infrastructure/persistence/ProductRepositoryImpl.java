87316025b7c01503b7680da41f714f2b24cbe173
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 18:33:45 +0530

    feat: get products from db realted to its id

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/persistence/ProductRepositoryImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/persistence/ProductRepositoryImpl.java
@@ -1,8 +1,12 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.models.Product;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.jpa.JpaProductRepository;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.persistenceMapper.ProductPersistenceMapper;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class ProductRepositoryImpl implements ProductRepository {
 
@@ -16,5 +20,12 @@ public class ProductRepositoryImpl implements ProductRepository {
     ) {
         this.jpaProductRepository = jpaProductRepository;
         this.productPersistenceMapper = productPersistenceMapper;
+    }
+
+    //product find by its id
+    @Override
+    public Optional<Product> productFindById(UUID productId) {
+        return jpaProductRepository.findByProductId(productId)
+                .map(productPersistenceMapper::toDomain);
     }
 }
