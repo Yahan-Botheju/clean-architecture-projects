a30ea0b3d8d3c632bbb263f58c6083d84b7275b3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 19:00:14 +0530

    feat: create catalog product check record class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/shared_domain/catalog/api_records/CatalogProductCheckResult.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/shared_domain/catalog/api_records/CatalogProductCheckResult.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records;
 
-public record CatalogProductCheckResult() {
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.enums.ProductStatus;
+
+import java.util.UUID;
+
+public record CatalogProductCheckResult(
+         UUID productId,
+         String productName,
+         double unitPrice,
+         int stockQuantity,
+         ProductStatus productStatus
+) {
 }
