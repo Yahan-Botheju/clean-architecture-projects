75b91a9fe47716c692d4ff1e3eb2bf5447a0cd61
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 19:25:02 +0530

    feat: implement create product response dto

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/DTOs/CreateProductResponseDTO.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/DTOs/CreateProductResponseDTO.java
@@ -1,4 +1,19 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.web.DTOs;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.enums.ProductStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateProductResponseDTO {
+    private UUID productId;
+    private String productName;
+    private double unitPrice;
+    private int stockQuantity;
+    private ProductStatus productStatus;
 }
