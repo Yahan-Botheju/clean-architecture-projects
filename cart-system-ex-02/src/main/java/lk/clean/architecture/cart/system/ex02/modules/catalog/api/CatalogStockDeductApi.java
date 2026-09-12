c2bfd9ae63b2fbf17728a320b211b895ce528f1e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 18:56:55 +0530

    feat: initiate method

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/api/CatalogStockDeductApi.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/api/CatalogStockDeductApi.java
@@ -1,4 +1,9 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.api;
 
+import java.util.UUID;
+
 public interface CatalogStockDeductApi {
+
+    //deduct quantity from stock
+    void deductStock(UUID productId, int quantity);
 }
