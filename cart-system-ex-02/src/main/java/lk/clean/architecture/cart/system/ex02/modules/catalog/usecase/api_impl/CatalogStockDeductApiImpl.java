31fad09e00db0e0a64570c820a28aa4cd9e909aa
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 20:48:10 +0530

    feat: create functionality of deduct stock api impl

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogStockDeductApiImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogStockDeductApiImpl.java
@@ -1,7 +1,11 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.api_impl;
 
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogStockDeductApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.models.Product;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class CatalogStockDeductApiImpl implements CatalogStockDeductApi {
 
@@ -10,5 +14,20 @@ public class CatalogStockDeductApiImpl implements CatalogStockDeductApi {
 
     public CatalogStockDeductApiImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    //deduct quantity
+    @Override
+    public void deductStock(UUID productId, int quantity) {
+        //check quantity is not empty
+        if(quantity <= 0){
+            throw new IllegalStateException("Quantity cannot be empty");
+        }
+
+        Product product = productRepository.productFindById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id " + productId));
+
+        //use domain logic
+        product.deductStock(quantity);
     }
 }
