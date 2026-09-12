0238b7b07ae2b145f287170d8af7e9ed164b926f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 18:25:57 +0530

    feat: initiate custom query

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/persistence/jpa/JpaProductRepository.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/persistence/jpa/JpaProductRepository.java
@@ -1,9 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.jpa;
 
 import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.models.Product;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.persistence.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaProductRepository extends JpaRepository<Product, UUID> {
+
+    //product find by id
+    Optional<ProductEntity>  findByProductId(UUID productId);
 }
