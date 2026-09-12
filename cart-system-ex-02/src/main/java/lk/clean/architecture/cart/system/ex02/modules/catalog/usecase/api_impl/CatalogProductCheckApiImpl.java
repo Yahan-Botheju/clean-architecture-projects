1ea6ad5de76e1b384c7c361dd7eeb24826fe94fd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 20:29:55 +0530

    feat: create functionality of catalog product check api impl

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogProductCheckApiImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/usecase/api_impl/CatalogProductCheckApiImpl.java
@@ -1,7 +1,12 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.api_impl;
 
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.models.Product;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.CatalogProductCheckResult;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class CatalogProductCheckApiImpl implements CatalogProductCheckApi {
 
@@ -10,5 +15,30 @@ public class CatalogProductCheckApiImpl implements CatalogProductCheckApi {
 
     public CatalogProductCheckApiImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    //check product availability
+    @Override
+    public CatalogProductCheckResult checkProductAvailability(UUID productId, int requestedQuantity) {
+        //check req.quantity is empty
+        if(requestedQuantity <= 0){
+            throw new IllegalStateException("Requested quantity cannot be empty");
+        }
+
+        Product getProduct = productRepository.productFindById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id " + productId));
+
+        //use domain logic
+        getProduct.enoughStockAvailable(requestedQuantity);
+
+        productRepository.saveProduct(getProduct);
+
+        return new CatalogProductCheckResult(
+                getProduct.getProductId(),
+                getProduct.getProductName(),
+                getProduct.getUnitPrice(),
+                getProduct.getStockQuantity(),
+                getProduct.getProductStatus()
+        );
     }
 }
