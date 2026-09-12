fb000e500acd26234134341fcd226874140a7a52
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-12 19:16:49 +0530

    feat: initiate catalog product check api impl as service class through config class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/configs/UseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,21 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.infrastructure.configs;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.domain.repositories.ProductRepository;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.api_impl.CatalogProductCheckApiImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    /* __EXTERNAL_API__ */
+
+
+    @Bean
+    public CatalogProductCheckApi catalogProductCheckApi(
+            ProductRepository productRepository
+    ) {
+        return new CatalogProductCheckApiImpl(productRepository);
+    }
 }
