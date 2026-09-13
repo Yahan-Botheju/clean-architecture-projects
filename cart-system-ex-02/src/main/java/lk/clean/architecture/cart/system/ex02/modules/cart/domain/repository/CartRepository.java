c12e1583819fcf29ec9e0d0ae0159706616feb7b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-13 22:13:57 +0530

    feat: initiate method

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/domain/repository/CartRepository.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/domain/repository/CartRepository.java
@@ -1,4 +1,11 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CartRepository {
+    //find user by id
+    Optional<Cart> findByUserId(UUID userId);
 }
