ef1be32102bf0f30801847936590bb5fecabaec3
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-13 18:55:27 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/controllers/ProductController.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/controllers/ProductController.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.web.controllers;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.CreateProductUseCase;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.web.webMappers.ProductWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    //inject required dependencies
+    private final CreateProductUseCase createProductUseCase;
+    private final ProductWebMapper productWebMapper;
+
+    public ProductController(CreateProductUseCase createProductUseCase, ProductWebMapper productWebMapper) {
+        this.createProductUseCase = createProductUseCase;
+        this.productWebMapper = productWebMapper;
+    }
 }
