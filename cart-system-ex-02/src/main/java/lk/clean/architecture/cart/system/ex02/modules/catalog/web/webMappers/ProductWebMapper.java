0ae99264a3d92e27f8d9576035cf7c20e995f84b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-13 18:48:59 +0530

    feat: implement create product web mapper

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/webMappers/ProductWebMapper.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/catalog/web/webMappers/ProductWebMapper.java
@@ -1,4 +1,17 @@
 package lk.clean.architecture.cart.system.ex02.modules.catalog.web.webMappers;
 
+import lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.records.CreateProductCommand;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.usecase.records.CreateProductResult;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.web.DTOs.CreateProductRequestDTO;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.web.DTOs.CreateProductResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ProductWebMapper {
+
+    //requestDTO to usecase obj
+    CreateProductCommand  toCreateProductCommand(CreateProductRequestDTO createProductRequestDTO);
+
+    //domain model to response dto
+    CreateProductResponseDTO toCreateProductResponseDTO(CreateProductResult createProductResult);
 }
