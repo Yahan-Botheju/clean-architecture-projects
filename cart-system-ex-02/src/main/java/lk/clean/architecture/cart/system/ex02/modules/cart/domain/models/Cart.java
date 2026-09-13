134478747414fb9516b936955b86dfb04ab486c5
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-13 20:22:09 +0530

    feat: create cart add item logic

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/domain/models/Cart.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/domain/models/Cart.java
@@ -9,19 +9,19 @@ import java.util.UUID;
 public class Cart {
     private final UUID cartId;
     private final UUID userId;
-    private List<CartItem> items;
+    private List<CartItem> cartItems;
     private double totalPrice;
 
-    public Cart(UUID cartId, UUID userId, List<CartItem> items, double totalPrice) {
+    public Cart(UUID cartId, UUID userId, List<CartItem> cartItems, double totalPrice) {
         this.cartId = cartId;
         this.userId = userId;
-        this.items = items;
+        this.cartItems = cartItems;
         this.totalPrice = totalPrice;
     }
 
     /* __FACTORY_METHOD__ */
 
-    public Cart createNewCart(
+    public static Cart createNewCart(
             UUID userId,
             List<CartItem> items,
             double totalPrice
@@ -33,5 +33,39 @@ public class Cart {
                 items,
                 totalPrice
         );
+    }
+
+    /* __DOMAIN_LOGIC__ */
+
+
+    //add item
+    public void addItem(UUID productId, String productName, double unitPrice, int requestedQuantity){
+
+        //check product is available
+        boolean checkCartItem = cartItems.stream().anyMatch(cartItem ->  cartItem.getProductId().equals(productId));
+
+        //check availability
+        if(checkCartItem){
+            //get product related to id and name
+            CartItem getItem = cartItems.stream()
+                    .filter( cartItem -> cartItem.getProductId()
+                            .equals(productId) && cartItem.getProductName().equals(productName))
+                    .findFirst()
+                    .orElseThrow(() ->  new IllegalArgumentException("Product not found"));
+
+            //add to price with existing one
+            getItem.updateQuantity(requestedQuantity);
+
+        }else{
+            //product not available create new cart item model
+            CartItem newCartItem = CartItem.createNewCartItem(
+                    productName,
+                    unitPrice,
+                    requestedQuantity
+            );
+            //add to list
+            cartItems.add(newCartItem);
+        }
+
     }
 }
