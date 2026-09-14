5ccf4b71189cf3a4c570ec5e1a7e460d744acd5c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 22:26:48 +0530

    feat: initiate update cart item usecase impl as service class through config class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/configs/UseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/configs/UseCaseBeanConfigs.java
@@ -3,6 +3,8 @@ package lk.clean.architecture.cart.system.ex02.modules.cart.infrastructure.confi
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.AddToCartUseCase;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.AddToCartUseCaseImpl;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.UpdateCartItemUseCase;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.UpdateCartItemUseCaseImpl;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +19,14 @@ public class UseCaseBeanConfigs {
             CatalogProductCheckApi catalogProductCheckApi
     ){
         return new AddToCartUseCaseImpl(cartRepository,catalogProductCheckApi);
+    }
+
+    //update cart item usecase impl
+    @Bean
+    public UpdateCartItemUseCase updateCartItemUseCase(
+            CartRepository cartRepository,
+            CatalogProductCheckApi catalogProductCheckApi
+    ){
+        return new UpdateCartItemUseCaseImpl(cartRepository,catalogProductCheckApi);
     }
 }
