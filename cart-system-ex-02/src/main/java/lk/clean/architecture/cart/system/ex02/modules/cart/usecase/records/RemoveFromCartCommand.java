c0ff7db3814e903a8fac05a5dd775791329869de
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 22:57:28 +0530

    feat: create remove item from cart command resutl record

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/records/RemoveFromCartCommand.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/records/RemoveFromCartCommand.java
@@ -1,4 +1,9 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records;
 
-public record RemoveFromCartCommand() {
+import java.util.UUID;
+
+public record RemoveFromCartCommand(
+        UUID userId,
+        UUID productId
+) {
 }
