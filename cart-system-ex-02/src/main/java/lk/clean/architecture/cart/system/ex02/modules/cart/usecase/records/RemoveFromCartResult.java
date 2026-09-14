b1ff2d87b43ba563042dec28f474237b7b75129a
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 23:01:21 +0530

    feat: create  remove from cart response result

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/records/RemoveFromCartResult.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/records/RemoveFromCartResult.java
@@ -1,4 +1,13 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records;
 
-public record RemoveFromCartResult() {
+import java.util.UUID;
+
+public record RemoveFromCartResult(
+        UUID productId,
+        String productName,
+        double unitPrice,
+        UUID cartId,
+        UUID userId
+
+) {
 }
