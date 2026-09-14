e4282977d4519c74b3f748bf6bf3b0d4fd91ec52
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 22:19:04 +0530

    feat: create add to cart functioncality

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/AddToCartUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/AddToCartUseCaseImpl.java
@@ -1,9 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.AddToCartCommand;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.AddToCartResult;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.CatalogProductCheckResult;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class AddToCartUseCaseImpl implements AddToCartUseCase {
 
@@ -19,11 +24,44 @@ public class AddToCartUseCaseImpl implements AddToCartUseCase {
         this.catalogProductCheckApi = catalogProductCheckApi;
     }
 
-
+    //add to item to cart
+    @Override
     public AddToCartResult execute(AddToCartCommand addToCartCommand) {
 
-        if(addToCartCommand.quantity() <= 0){
-            throw new IllegalArgumentException("Quantity must be greater than zero.");
+        //validate incoming quantity
+        if( addToCartCommand.userId() == null || addToCartCommand.productId() == null ||addToCartCommand.quantity() <= 0){
+            throw new IllegalArgumentException("Required parameters missing, try again");
         }
+
+        //check product availability and  has enough stock
+        CatalogProductCheckResult productSnapshot = catalogProductCheckApi.checkProductAvailability(addToCartCommand.productId(), addToCartCommand.quantity());
+
+        //get user cart
+        Optional<Cart> getCart = cartRepository.findByUserId(addToCartCommand.userId());
+
+        //if cart not available create new one or use existence one
+        Cart cart = getCart.orElseGet(() -> Cart.createNewCart(
+                addToCartCommand.userId(),
+                new ArrayList<>(),
+                0.0
+        ));
+
+        //add values to cart by calling domain logic
+        cart.addItem(
+                addToCartCommand.productId(),
+                productSnapshot.productName(),
+                productSnapshot.unitPrice(),
+                addToCartCommand.quantity()
+        );
+
+        //save in db
+        cartRepository.save(cart);
+
+        return new AddToCartResult(
+                cart.getCartId(),
+                cart.getUserId(),
+                cart.getCartItems(),
+                cart.getTotalPrice()
+        );
     }
 }
