6e2d38128a904a8b0af8631f6b11d00e175c410f
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 22:53:04 +0530

    feat: initiate method

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/UpdateCartItemUseCase.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/UpdateCartItemUseCase.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.UpdateCartItemCommand;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.UpdateCartItemResult;
+
 public interface UpdateCartItemUseCase {
+
+    //update cart item
+    UpdateCartItemResult execute(UpdateCartItemCommand updateCartItemCommand);
 }
