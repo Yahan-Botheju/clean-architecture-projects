3815e91569ed7db54f5be2c60e2ada62b984188b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 22:53:38 +0530

    feat: create functinality of update cart item usecase impl

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/UpdateCartItemUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/UpdateCartItemUseCaseImpl.java
@@ -1,7 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.UpdateCartItemCommand;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.UpdateCartItemResult;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.CatalogProductCheckResult;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.Optional;
 
 public class UpdateCartItemUseCaseImpl implements UpdateCartItemUseCase {
 
@@ -15,5 +22,32 @@ public class UpdateCartItemUseCaseImpl implements UpdateCartItemUseCase {
     ) {
         this.cartRepository = cartRepository;
         this.catalogProductCheckApi = catalogProductCheckApi;
+    }
+
+    //update cart item
+    @Override
+    public UpdateCartItemResult execute(UpdateCartItemCommand updateCartItemCommand) {
+
+        //check for new request quantity has available stock
+        CatalogProductCheckResult productSnapshots = catalogProductCheckApi.checkProductAvailability(
+                updateCartItemCommand.productId(),
+                updateCartItemCommand.newQuantity()
+        );
+
+        //get cart related to user id
+        Cart cart = cartRepository.findByUserId(updateCartItemCommand.userId())
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+
+        //call domain method
+        cart.updateItemQuantity(updateCartItemCommand.productId(), updateCartItemCommand.newQuantity());
+
+        return new UpdateCartItemResult(
+                updateCartItemCommand.productId(),
+                productSnapshots.productName(),
+                productSnapshots.unitPrice(),
+                updateCartItemCommand.newQuantity()
+        );
     }
 }
