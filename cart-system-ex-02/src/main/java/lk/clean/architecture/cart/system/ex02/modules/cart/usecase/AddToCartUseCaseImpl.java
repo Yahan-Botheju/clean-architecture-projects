6009e2c5499a33771fbc302e2180fd820f1509b8
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 21:04:43 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/AddToCartUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/AddToCartUseCaseImpl.java
@@ -1,4 +1,29 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
-public class AddToCartUseCaseImpl {
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.AddToCartCommand;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.AddToCartResult;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+
+public class AddToCartUseCaseImpl implements AddToCartUseCase {
+
+    //inject required dependencies
+    private final CartRepository cartRepository;
+    private final CatalogProductCheckApi catalogProductCheckApi;
+
+    public AddToCartUseCaseImpl(
+            CartRepository cartRepository,
+            CatalogProductCheckApi catalogProductCheckApi
+    ) {
+        this.cartRepository = cartRepository;
+        this.catalogProductCheckApi = catalogProductCheckApi;
+    }
+
+
+    public AddToCartResult execute(AddToCartCommand addToCartCommand) {
+
+        if(addToCartCommand.quantity() <= 0){
+            throw new IllegalArgumentException("Quantity must be greater than zero.");
+        }
+    }
 }
