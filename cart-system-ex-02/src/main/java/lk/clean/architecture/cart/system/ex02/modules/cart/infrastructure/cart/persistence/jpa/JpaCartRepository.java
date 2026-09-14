9177b7fe2f985605560727d2356b134d7348eed2
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-14 20:29:14 +0530

    feat: create custom query

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/cart/persistence/jpa/JpaCartRepository.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/cart/persistence/jpa/JpaCartRepository.java
@@ -3,7 +3,10 @@ package lk.clean.architecture.cart.system.ex02.modules.cart.infrastructure.cart.
 import lk.clean.architecture.cart.system.ex02.modules.cart.infrastructure.cart.persistence.entities.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaCartRepository extends JpaRepository<CartEntity , UUID> {
+
+    Optional<CartEntity> findByUserId(UUID userId);
 }
