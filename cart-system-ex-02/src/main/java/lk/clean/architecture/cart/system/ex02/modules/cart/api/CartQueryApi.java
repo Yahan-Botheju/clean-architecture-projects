ae4444ca3a70b104363390efed947fd9d0200a6b
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 12:45:08 +0530

    feat: initiate method

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/api/CartQueryApi.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/api/CartQueryApi.java
@@ -1,4 +1,10 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.api;
 
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.CartDetailsResult;
+
+import java.util.UUID;
+
 public interface CartQueryApi {
+
+    CartDetailsResult getCartByUserId(UUID userId);
 }
