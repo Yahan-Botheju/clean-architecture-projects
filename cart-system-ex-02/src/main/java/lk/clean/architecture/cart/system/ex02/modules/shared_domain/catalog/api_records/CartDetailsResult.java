2102cc078077a54d1eca41b18d0e40f8031ec291
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 12:49:05 +0530

    initiate return fields temporarly

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/shared_domain/catalog/api_records/CartDetailsResult.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/shared_domain/catalog/api_records/CartDetailsResult.java
@@ -1,4 +1,14 @@
 package lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records;
 
-public record CartDetailsResult() {
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.CartItem;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CartDetailsResult(
+        UUID cartId,
+        UUID userId,
+        List<CartItem>cartItems,
+        double totalPrice
+) {
 }
