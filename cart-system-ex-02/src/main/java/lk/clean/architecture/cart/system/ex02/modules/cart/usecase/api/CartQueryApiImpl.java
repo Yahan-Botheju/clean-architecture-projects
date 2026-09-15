2b5c460e9c27e4cb81ea7d3a4b51352893af8f10
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 12:59:32 +0530

    feat: create get cart details by user id functionality

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/api/CartQueryApiImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/api/CartQueryApiImpl.java
@@ -1,7 +1,12 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase.api;
 
 import lk.clean.architecture.cart.system.ex02.modules.cart.api.CartQueryApi;
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.CartDetailsResult;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class CartQueryApiImpl implements CartQueryApi {
 
@@ -10,5 +15,20 @@ public class CartQueryApiImpl implements CartQueryApi {
 
     public CartQueryApiImpl(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
+    }
+
+    //cart details
+    @Override
+    public CartDetailsResult getCartByUserId(UUID userId) {
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+
+        return new CartDetailsResult(
+                cart.getCartId(),
+                cart.getUserId(),
+                cart.getCartItems(),
+                cart.getTotalPrice()
+        );
     }
 }
