2e4dd3858b191071b6b590276ee5164177a6abf9
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 11:34:55 +0530

    fix: remove existing dependency and inject correct one

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/RemoveFromCartUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/RemoveFromCartUseCaseImpl.java
@@ -1,19 +1,44 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
-import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogProductCheckApi;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.RemoveFromCartCommand;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.RemoveFromCartResult;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.GetProductDetailsApi;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.Optional;
 
 public class RemoveFromCartUseCaseImpl implements RemoveFromCartUseCase {
 
     //inject required dependencies
     private final CartRepository cartRepository;
-    private final CatalogProductCheckApi catalogProductCheckApi;
+    private final GetProductDetailsApi getProductDetailsApi;
 
     public RemoveFromCartUseCaseImpl(
             CartRepository cartRepository,
-            CatalogProductCheckApi catalogProductCheckApi
+            GetProductDetailsApi getProductDetailsApi
     ) {
         this.cartRepository = cartRepository;
-        this.catalogProductCheckApi = catalogProductCheckApi;
+        this.getProductDetailsApi = getProductDetailsApi;
+    }
+
+    public RemoveFromCartResult removeFromCart(RemoveFromCartCommand removeFromCartCommand) {
+
+        if(removeFromCartCommand.userId() == null || removeFromCartCommand.productId() == null){
+            throw new ResourceNotFoundException("Required fields are empty");
+        }
+
+
+
+       Optional<Cart> getCart = cartRepository.findByUserId(removeFromCartCommand.userId());
+
+        Cart cart = getCart.orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+
+       cart.removeItem(removeFromCartCommand.productId());
+
+       cartRepository.save(cart);
+
+
     }
 }
