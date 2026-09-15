33e38b17e07abf0c954cf4b7b14d80ba83b5b870
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 11:41:52 +0530

    feat: create functionality of remove from cart

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/RemoveFromCartUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/RemoveFromCartUseCaseImpl.java
@@ -5,6 +5,7 @@ import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.Car
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.RemoveFromCartCommand;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.RemoveFromCartResult;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.GetProductDetailsApi;
+import lk.clean.architecture.cart.system.ex02.modules.shared_domain.catalog.api_records.GetProductDetailsResult;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.util.Optional;
@@ -23,22 +24,32 @@ public class RemoveFromCartUseCaseImpl implements RemoveFromCartUseCase {
         this.getProductDetailsApi = getProductDetailsApi;
     }
 
+    //remove from cart
+    @Override
     public RemoveFromCartResult removeFromCart(RemoveFromCartCommand removeFromCartCommand) {
 
-        if(removeFromCartCommand.userId() == null || removeFromCartCommand.productId() == null){
+        //check incoming fields are empty
+        if (removeFromCartCommand.userId() == null || removeFromCartCommand.productId() == null) {
             throw new ResourceNotFoundException("Required fields are empty");
         }
+        //get product
+        GetProductDetailsResult productSnapshots = getProductDetailsApi.getProductDetails(removeFromCartCommand.productId());
 
-
-
-       Optional<Cart> getCart = cartRepository.findByUserId(removeFromCartCommand.userId());
-
+        //get cart related to user
+        Optional<Cart> getCart = cartRepository.findByUserId(removeFromCartCommand.userId());
         Cart cart = getCart.orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
 
-       cart.removeItem(removeFromCartCommand.productId());
+        //call domain model for remove item from cart
+        cart.removeItem(removeFromCartCommand.productId());
 
-       cartRepository.save(cart);
+        cartRepository.save(cart);
 
-
+        return new RemoveFromCartResult(
+                productSnapshots.productId(),
+                productSnapshots.productName(),
+                productSnapshots.unitPrice(),
+                cart.getCartId(),
+                cart.getUserId()
+        );
     }
 }
