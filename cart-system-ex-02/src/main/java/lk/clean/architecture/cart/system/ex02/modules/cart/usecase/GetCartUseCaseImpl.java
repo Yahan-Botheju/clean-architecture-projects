c97e22e73fb959c1fb7ee680595c4e9c1cf49646
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 12:20:02 +0530

    feat: create functionality of get cart usecase impl

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/GetCartUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/GetCartUseCaseImpl.java
@@ -1,6 +1,11 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.domain.models.Cart;
 import lk.clean.architecture.cart.system.ex02.modules.cart.domain.repository.CartRepository;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.GetCartResult;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+import java.util.UUID;
 
 public class GetCartUseCaseImpl implements GetCartUseCase {
 
@@ -9,5 +14,20 @@ public class GetCartUseCaseImpl implements GetCartUseCase {
 
     public GetCartUseCaseImpl(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
+    }
+
+    //get cart
+    @Override
+    public GetCartResult getCart(UUID userId) {
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found!"));
+
+        return new GetCartResult(
+                cart.getCartId(),
+                cart.getUserId(),
+                cart.getCartItems(),
+                cart.getTotalPrice()
+        );
     }
 }
