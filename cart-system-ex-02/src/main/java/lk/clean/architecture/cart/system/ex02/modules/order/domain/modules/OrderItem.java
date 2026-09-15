af1af4ded7df5bb23d95fe3042d39414ed2b8f95
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-15 13:20:26 +0530

    feat: create order item domain model

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/modules/OrderItem.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/modules/OrderItem.java
@@ -1,4 +1,20 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.domain.modules;
 
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
 public class OrderItem {
+    private final UUID productId;
+    private String productName;
+    private double unitPrice;
+    private int quantity;
+
+    public OrderItem(UUID productId, String productName, double unitPrice, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
 }
