edf509d3bb8f2706ee8dd828aa108e5bbbfd59fb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 20:13:05 +0530

    feat: initiate place order usecase impl as service class through config class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/UseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/UseCaseBeanConfigs.java
@@ -1,4 +1,23 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.configs;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.api.CartQueryApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogStockDeductApi;
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.PlaceOrderUseCase;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.PlaceOrderUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseBeanConfigs {
+
+    //place order usecase impl
+    @Bean
+    public PlaceOrderUseCase placeOrderUseCase(
+            OrderRepository orderRepository,
+            CartQueryApi cartQueryApi,
+            CatalogStockDeductApi catalogStockDeductApi
+    ) {
+        return new PlaceOrderUseCaseImpl(orderRepository, cartQueryApi, catalogStockDeductApi);
+    }
 }
