da0b05ca676bf827ec7d7220cb84275683501385
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 19:52:06 +0530

    feat: custom query

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/persistence/jpa/JpaOrderRepository.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/persistence/jpa/JpaOrderRepository.java
@@ -3,7 +3,10 @@ package lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.pers
 import lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.persistence.entities.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaOrderRepository extends JpaRepository<OrderEntity, UUID> {
+
+    Optional<OrderEntity> findByOrderId(UUID orderId);
 }
