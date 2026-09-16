46355f3024eea68f84837e7fe7b0ab83adb7642e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 19:48:28 +0530

    feat: save order in db under required process

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/persistence/OrderRepositoryImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/persistence/OrderRepositoryImpl.java
@@ -1,6 +1,8 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.persistence;
 
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.modules.Order;
 import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+import lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.persistence.entities.OrderEntity;
 import lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.persistence.jpa.JpaOrderRepository;
 import lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.persistence.persistenceMapper.OrderPersistenceMapper;
 
@@ -13,5 +15,16 @@ public class OrderRepositoryImpl implements OrderRepository {
     public OrderRepositoryImpl(JpaOrderRepository jpaOrderRepository, OrderPersistenceMapper orderPersistenceMapper) {
         this.jpaOrderRepository = jpaOrderRepository;
         this.orderPersistenceMapper = orderPersistenceMapper;
+    }
+
+
+    //save order
+    @Override
+    public Order save(Order order) {
+
+        OrderEntity toOrderEntity = orderPersistenceMapper.toOrderEntity(order);
+        OrderEntity savedOrderEntity = jpaOrderRepository.save(toOrderEntity);
+
+        return orderPersistenceMapper.toOrderDomainModel(savedOrderEntity);
     }
 }
