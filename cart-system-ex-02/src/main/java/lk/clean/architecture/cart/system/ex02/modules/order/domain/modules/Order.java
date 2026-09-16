69188fa3bfd844bbe37c67ed408377d3be78721e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 18:59:45 +0530

    feat: create order domain

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/modules/Order.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/modules/Order.java
@@ -1,4 +1,27 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.domain.modules;
 
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.enums.OrderStatus;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
 public class Order {
+    private final UUID orderId;
+    private final UUID userId;
+    private double totalAmount;
+    private OrderStatus orderStatus;
+    private List<OrderItem> orderItems;
+    private LocalDateTime createdAt;
+
+    public Order(UUID orderId, UUID userId, double totalAmount, OrderStatus orderStatus, List<OrderItem> orderItems, LocalDateTime createdAt) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.totalAmount = totalAmount;
+        this.orderStatus = orderStatus;
+        this.orderItems = orderItems;
+        this.createdAt = createdAt;
+    }
 }
