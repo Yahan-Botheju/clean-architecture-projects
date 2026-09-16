1cb849750c010013d26750d51fbc44069cd2fe18
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 20:08:34 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/PlaceOrderUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/PlaceOrderUseCaseImpl.java
@@ -1,4 +1,23 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.usecase;
 
-public class PlaceOrderUseCaseImpl {
+import lk.clean.architecture.cart.system.ex02.modules.cart.api.CartQueryApi;
+import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogStockDeductApi;
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+
+public class PlaceOrderUseCaseImpl implements PlaceOrderUseCase {
+
+    //inject required dependencies
+    private final OrderRepository orderRepository;
+    private final CartQueryApi cartQueryApi;
+    private final CatalogStockDeductApi catalogStockDeductApi;
+
+    public PlaceOrderUseCaseImpl(
+            OrderRepository orderRepository,
+            CartQueryApi cartQueryApi,
+            CatalogStockDeductApi catalogStockDeductApi
+    ) {
+        this.orderRepository = orderRepository;
+        this.cartQueryApi = cartQueryApi;
+        this.catalogStockDeductApi = catalogStockDeductApi;
+    }
 }
