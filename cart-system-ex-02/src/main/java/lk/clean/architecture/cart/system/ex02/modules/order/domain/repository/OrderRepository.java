9354fd9d687cf2ea34ed54ae175770339cd1465e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-16 19:57:53 +0530

    feat: initiate method

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/repository/OrderRepository.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/domain/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package lk.clean.architecture.cart.system.ex02.modules.order.domain.repository;
 
 import lk.clean.architecture.cart.system.ex02.modules.order.domain.modules.Order;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,6 +10,9 @@ public interface OrderRepository {
 
     //order find by id
     Optional<Order> getOrderByOrderId(UUID orderId);
+
+    //get order list related to user id
+    List<Order> getOrderByUserId(UUID userId);
 
     //save order
     Order save(Order order);
