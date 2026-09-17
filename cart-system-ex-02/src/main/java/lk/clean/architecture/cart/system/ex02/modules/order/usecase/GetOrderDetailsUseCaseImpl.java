19ff79219947729a4a7fb08eeb491ef143cc2e07
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:29:26 +0530

    feat: create get order details usecase impl functionality

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/GetOrderDetailsUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/GetOrderDetailsUseCaseImpl.java
@@ -1,6 +1,11 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.modules.Order;
 import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.records.GetOrderDetailsCommand;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.records.GetOrderDetailsResult;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
 
 public class GetOrderDetailsUseCaseImpl implements  GetOrderDetailsUseCase {
 
@@ -9,5 +14,24 @@ public class GetOrderDetailsUseCaseImpl implements  GetOrderDetailsUseCase {
 
     public GetOrderDetailsUseCaseImpl(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
+    }
+
+    //get user placed order details
+    @Override
+    public GetOrderDetailsResult getOrderDetails(GetOrderDetailsCommand getOrderDetailsCommand) {
+        //get order check existence
+         Order  getOrder = orderRepository.getOrderByUserId(getOrderDetailsCommand.userId())
+                 .stream()
+                 .findFirst()
+                 .orElseThrow(() -> new ResourceNotFoundException("Order not found related to user ID"));
+
+        return new GetOrderDetailsResult(
+             getOrder.getOrderId(),
+             getOrder.getUserId(),
+             getOrder.getTotalAmount(),
+             getOrder.getOrderStatus(),
+             getOrder.getOrderItems(),
+             getOrder.getCreatedAt()
+        );
     }
 }
