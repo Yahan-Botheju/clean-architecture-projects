445c32681b9bebf7a695f6c31725e590786c998c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:00:13 +0530

    feat: initiate get order usecase impl as service class through config class

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/UseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/UseCaseBeanConfigs.java
@@ -3,6 +3,8 @@ package lk.clean.architecture.cart.system.ex02.modules.order.infrastructure.conf
 import lk.clean.architecture.cart.system.ex02.modules.cart.api.CartQueryApi;
 import lk.clean.architecture.cart.system.ex02.modules.catalog.api.CatalogStockDeductApi;
 import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.GetOrderDetailsUseCase;
+import lk.clean.architecture.cart.system.ex02.modules.order.usecase.GetOrderDetailsUseCaseImpl;
 import lk.clean.architecture.cart.system.ex02.modules.order.usecase.PlaceOrderUseCase;
 import lk.clean.architecture.cart.system.ex02.modules.order.usecase.PlaceOrderUseCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -19,5 +21,13 @@ public class UseCaseBeanConfigs {
             CatalogStockDeductApi catalogStockDeductApi
     ) {
         return new PlaceOrderUseCaseImpl(orderRepository, cartQueryApi, catalogStockDeductApi);
+    }
+
+    //get order details usecase impl
+    @Bean
+    public GetOrderDetailsUseCase getOrderDetailsUseCase(
+            OrderRepository orderRepository
+    ){
+        return new GetOrderDetailsUseCaseImpl(orderRepository);
     }
 }
