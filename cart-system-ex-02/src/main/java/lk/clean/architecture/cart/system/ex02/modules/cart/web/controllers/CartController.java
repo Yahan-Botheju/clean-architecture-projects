2ddcee01ab95a6e728ff9e915aa3fc9315fd015e
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:46:15 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/web/controllers/CartController.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/web/controllers/CartController.java
@@ -1,9 +1,17 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.web.controllers;
 
+import jakarta.validation.Valid;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.AddToCartUseCase;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.GetCartUseCase;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.RemoveFromCartUseCase;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.UpdateCartItemUseCase;
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.AddToCartCommand;
+import lk.clean.architecture.cart.system.ex02.modules.cart.web.DTOs.AddToCartRequestDTO;
+import lk.clean.architecture.cart.system.ex02.modules.cart.web.DTOs.AddToCartResponseDTO;
+import lk.clean.architecture.cart.system.ex02.modules.cart.web.webMappers.CartWebMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +24,26 @@ public class CartController {
     private final GetCartUseCase getCartUseCase;
     private final UpdateCartItemUseCase updateCartItemUseCase;
     private final RemoveFromCartUseCase removeFromCartUseCase;
+    private final CartWebMapper cartWebMapper;
 
     public CartController(
             AddToCartUseCase addToCartUseCase,
             GetCartUseCase getCartUseCase,
             UpdateCartItemUseCase updateCartItemUseCase,
-            RemoveFromCartUseCase removeFromCartUseCase
+            RemoveFromCartUseCase removeFromCartUseCase,
+            CartWebMapper cartWebMapper
     ) {
         this.addToCartUseCase = addToCartUseCase;
         this.getCartUseCase = getCartUseCase;
         this.updateCartItemUseCase = updateCartItemUseCase;
         this.removeFromCartUseCase = removeFromCartUseCase;
+        this.cartWebMapper = cartWebMapper;
+    }
+
+    @PostMapping("/add-to-cart")
+    public ResponseEntity<AddToCartResponseDTO> addToCart(
+            @Valid @RequestBody AddToCartRequestDTO addToCartRequestDTO
+    ){
+        AddToCartCommand toAddToCartCommand =
     }
 }
