939ed0d134ecda185df36ea7ad3b48c2e51ac169
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:57:08 +0530

    ref: update params

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/GetCartUseCase.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/usecase/GetCartUseCase.java
@@ -1,5 +1,6 @@
 package lk.clean.architecture.cart.system.ex02.modules.cart.usecase;
 
+import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.GetCartCommand;
 import lk.clean.architecture.cart.system.ex02.modules.cart.usecase.records.GetCartResult;
 
 import java.util.UUID;
@@ -7,5 +8,5 @@ import java.util.UUID;
 public interface GetCartUseCase {
 
     //get cart
-    GetCartResult getCart(UUID userId);
+    GetCartResult getCart(GetCartCommand getCartCommand);
 }
