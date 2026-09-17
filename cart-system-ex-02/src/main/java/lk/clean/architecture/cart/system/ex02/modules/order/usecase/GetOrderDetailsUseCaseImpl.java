f655bc90e901641c81c030a5141b654d77cd53bd
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:00:50 +0530

    feat: inject required dependencies

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/GetOrderDetailsUseCaseImpl.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/usecase/GetOrderDetailsUseCaseImpl.java
@@ -1,4 +1,13 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.usecase;
 
-public class GetOrderDetailsUseCaseImpl {
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.repository.OrderRepository;
+
+public class GetOrderDetailsUseCaseImpl implements  GetOrderDetailsUseCase {
+
+    //inject required dependencies
+    private final OrderRepository orderRepository;
+
+    public GetOrderDetailsUseCaseImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 }
