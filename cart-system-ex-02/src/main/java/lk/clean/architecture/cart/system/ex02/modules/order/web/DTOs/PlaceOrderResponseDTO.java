39c00789e2310b52e97ae0efe82ab35dca7993d6
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-17 18:13:57 +0530

    feat: create place order response dto

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/web/DTOs/PlaceOrderResponseDTO.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/web/DTOs/PlaceOrderResponseDTO.java
@@ -1,4 +1,23 @@
 package lk.clean.architecture.cart.system.ex02.modules.order.web.DTOs;
 
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.enums.OrderStatus;
+import lk.clean.architecture.cart.system.ex02.modules.order.domain.modules.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlaceOrderResponseDTO {
+    private UUID orderId;
+    private UUID userId;
+    private double totalAmount;
+    private OrderStatus orderStatus;
+    private List<OrderItem> orderItems;
+    private LocalDateTime createdAt;
 }
