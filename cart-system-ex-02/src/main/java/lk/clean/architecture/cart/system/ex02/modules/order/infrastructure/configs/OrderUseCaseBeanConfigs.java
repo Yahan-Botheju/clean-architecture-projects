d97bf7d6c287f5d6d5c3ea7798093634a47d24eb
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-18 20:11:12 +0530

    ref: change class name

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/OrderUseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/order/infrastructure/configs/OrderUseCaseBeanConfigs.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UseCaseBeanConfigs {
+public class OrderUseCaseBeanConfigs {
 
     //place order usecase impl
     @Bean
