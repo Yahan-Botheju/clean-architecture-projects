b314c19cbe349036405d1a555e3150b0f0066d8d
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-09-18 20:10:20 +0530

    ref: change class name

--- cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/configs/CartUseCaseBeanConfigs.java
+++ cart-system-ex-02/src/main/java/lk/clean/architecture/cart/system/ex02/modules/cart/infrastructure/configs/CartUseCaseBeanConfigs.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UseCaseBeanConfigs {
+public class CartUseCaseBeanConfigs {
 
     /* __EXTERNAL_API__ */
 
